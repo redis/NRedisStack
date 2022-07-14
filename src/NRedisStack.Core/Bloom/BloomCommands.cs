1a5f0256da3d918da62209fe110d24072accf9ea
shacharPash <s.pashchur@gmail.com>
2022-07-14 18:11:18 +0300

    Add RedisBloom commands & Verify the creation of only one instance of each commands class

--- src/NRedisStack.Core/Bloom/BloomCommands.cs
+++ src/NRedisStack.Core/Bloom/BloomCommands.cs
@@ -11,14 +11,34 @@ namespace NRedisStack.Core
             _db = db;
         }
 
-
         public RedisResult Add(RedisKey key, string item)
         {
             return _db.Execute(BF.ADD, key, item);
         }
+
         public RedisResult Exists(RedisKey key, string item)
         {
             return _db.Execute(BF.EXISTS, key, item);
+        }
+
+        public RedisResult Info(RedisKey key)
+        {
+            return _db.Execute(BF.INFO, key);
+        }
+
+        public RedisResult Insert(RedisKey key)
+        {
+            return _db.Execute(BF.INFO, key);
+        }
+
+        public RedisResult ScanDump(RedisKey key, int iterator)
+        {
+            return _db.Execute(BF.SCANDUMP, key, iterator);
+        }
+
+        public RedisResult LoadChunk(RedisKey key, int iterator, string data)
+        {
+            return _db.Execute(BF.INFO, key, iterator, data);
         }
 
     }
