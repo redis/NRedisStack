5d922e97825ca87f99947a64672f2ed6de0e0089
shacharPash <93581407+shacharPash@users.noreply.github.com>
2022-08-28 17:42:30 +0300

    Add Tests for [BUCKETTIMESTAMP bt] [EMPTY] features (#26)
    * Add Tests for [BUCKETTIMESTAMP bt] [EMPTY] features
    
    * Fix UpperCase problem in AggregationExtensions

--- src/NRedisStack/TimeSeries/Extensions/BucketTimestampsExtensions.cs
+++ src/NRedisStack/TimeSeries/Extensions/BucketTimestampsExtensions.cs
@@ -7,17 +7,17 @@ namespace NRedisStack.Extensions
     {
         public static string AsArg(this TsBucketTimestamps bt) => bt switch
         {
-            TsBucketTimestamps.low => "low",
-            TsBucketTimestamps.mid => "mid",
-            TsBucketTimestamps.high => "high",
+            TsBucketTimestamps.low => "-",
+            TsBucketTimestamps.mid => "~",
+            TsBucketTimestamps.high => "+",
             _ => throw new ArgumentOutOfRangeException(nameof(bt), "Invalid TsBucketTimestamps type"),
         };
 
         public static TsBucketTimestamps Asbt(string bt) => bt switch
         {
-            "low" => TsBucketTimestamps.low,
-            "mid" => TsBucketTimestamps.mid,
-            "high" => TsBucketTimestamps.high,
+            "-" => TsBucketTimestamps.low,
+            "~" => TsBucketTimestamps.mid,
+            "+" => TsBucketTimestamps.high,
             _ => throw new ArgumentOutOfRangeException(nameof(bt), $"Invalid TsBucketTimestamps type '{bt}'"),
         };
     }
