56ef34a17b36755ff82432d2053494b1a0190c2e
Shachar Pashchur <s.pashchur@gmail.com>
2022-11-16 17:34:18 +0200

    Creating ExecuteInTransaction function

--- tests/NRedisStack.Tests/RedisFixture.cs
+++ tests/NRedisStack.Tests/RedisFixture.cs
@@ -5,6 +5,8 @@ namespace NRedisStack.Tests
 {
     public class RedisFixture : IDisposable
     {
+        private readonly IDatabase _db; // TODO: Check if its right to have this here
+
         public RedisFixture() => Redis = ConnectionMultiplexer.Connect("localhost");
 
         public void Dispose()
@@ -13,5 +15,25 @@ namespace NRedisStack.Tests
         }
 
         public ConnectionMultiplexer Redis { get; private set; }
+
+        /// <summary>
+        /// Executes the contained commands within the context of a transaction.
+        /// </summary>
+        /// <param name="commandArgsTuples">each tuple represents a command and
+        ///     it's arguments to execute inside a transaction.</param>
+        /// <returns>A RedisResult.</returns>
+        public RedisResult[] ExecuteInTransaction(Tuple<string, string[]>[] commandArgsTuples)
+        {
+            var transaction = _db.CreateTransaction();
+            var tasks = new List<Task<RedisResult>>();
+            foreach (var tuple in commandArgsTuples)
+            {
+                tasks.Add(transaction.ExecuteAsync(tuple.Item1, tuple.Item2));
+            }
+
+            transaction.Execute();
+            Task.WhenAll(tasks).Wait();
+            return tasks.Select(x => x.Result).ToArray();
+        }
     }
 }
