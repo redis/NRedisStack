dedd93f1c7b6669e5de1990d4dbdaf590be441ba
Shachar Pashchur <s.pashchur@gmail.com>
2022-11-21 15:02:33 +0200

    Fix SerializedCommand

--- src/NRedisStack/SerializedCommand.cs
+++ src/NRedisStack/SerializedCommand.cs
@@ -2,13 +2,19 @@ namespace NRedisStack.RedisStackCommands
 {
     public class SerializedCommand
     {
+        public string Command { get; }
+        public object[] Args { get; }
+
         public SerializedCommand(string command, params object[] args)
         {
             Command = command;
             Args = args;
         }
 
-        public string Command { get; }
-        public object[] Args { get; }
+        public SerializedCommand(string command, ICollection<object> args)
+        {
+            Command = command;
+            Args = args.ToArray();
+        }
     }
 }
