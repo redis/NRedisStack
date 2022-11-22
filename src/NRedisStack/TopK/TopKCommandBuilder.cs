ca97a3f102cf462f035599e99d8af2b7b9f6cfe3
Shachar Pashchur <s.pashchur@gmail.com>
2022-11-22 11:58:17 +0200

    Add TopKCommandBuilder

---
+++ src/NRedisStack/TopK/TopKCommandBuilder.cs
@@ -0,0 +1,72 @@
+using NRedisStack.Literals;
+using StackExchange.Redis;
+using NRedisStack.RedisStackCommands;
+
+namespace NRedisStack
+{
+    public static class TopKCommandBuilder
+    {
+        public static SerializedCommand Add(RedisKey key, params RedisValue[] items)
+        {
+            if (items.Length < 1)
+                throw new ArgumentOutOfRangeException(nameof(items));
+
+            var args = Auxiliary.MergeArgs(key, items);
+            return new SerializedCommand(TOPK.ADD, args);
+        }
+
+        public static SerializedCommand Count(RedisKey key, params RedisValue[] items)
+        {
+            if (items.Length < 1)
+                throw new ArgumentOutOfRangeException(nameof(items));
+
+            var args = Auxiliary.MergeArgs(key, items);
+            return new SerializedCommand(TOPK.COUNT, args);
+        }
+
+        public static SerializedCommand IncrBy(RedisKey key, params Tuple<RedisValue, long>[] itemIncrements)
+        {
+            if (itemIncrements.Length < 1)
+                throw new ArgumentOutOfRangeException(nameof(itemIncrements));
+
+            List<object> args = new List<object> { key };
+            foreach (var pair in itemIncrements)
+            {
+                args.Add(pair.Item1);
+                args.Add(pair.Item2);
+            }
+            return new SerializedCommand(TOPK.INCRBY, args);
+        }
+
+        public static SerializedCommand Info(RedisKey key)
+        {
+             return new SerializedCommand(TOPK.INFO, key);
+        }
+
+        public static SerializedCommand List(RedisKey key, bool withcount = false)
+        {
+            return (withcount) ? new SerializedCommand(TOPK.LIST, key, "WITHCOUNT")
+                                     : new SerializedCommand(TOPK.LIST, key);
+        }
+
+        public static SerializedCommand Query(RedisKey key, RedisValue item)
+        {
+            return new SerializedCommand(TOPK.QUERY, key, item);
+        }
+
+        public static SerializedCommand Query(RedisKey key, params RedisValue[] items)
+        {
+            if (items.Length < 1)
+                throw new ArgumentOutOfRangeException(nameof(items));
+
+            var args = Auxiliary.MergeArgs(key, items);
+
+            return new SerializedCommand(TOPK.QUERY, args);
+        }
+
+        public static SerializedCommand Reserve(RedisKey key, long topk, long width = 7, long depth = 8, double decay = 0.9)
+        {
+            return new SerializedCommand(TOPK.RESERVE, key, topk, width, depth, decay);
+        }
+    }
+}
--- src/NRedisStack/TopK/TopKCommands.cs
+++ src/NRedisStack/TopK/TopKCommands.cs
@@ -15,156 +15,98 @@ namespace NRedisStack
         /// <inheritdoc/>
         public RedisResult[]? Add(RedisKey key, params RedisValue[] items)
         {
-            if (items.Length < 1)
-                throw new ArgumentOutOfRangeException(nameof(items));
-
-            var args = Auxiliary.MergeArgs(key, items);
-            return (RedisResult[]?)_db.Execute(TOPK.ADD, args);
+            return (RedisResult[]?) _db.Execute(TopKCommandBuilder.Add(key, items));
         }
 
         /// <inheritdoc/>
         public async Task<RedisResult[]?> AddAsync(RedisKey key, params RedisValue[] items)
         {
-            if (items.Length < 1)
-                throw new ArgumentOutOfRangeException(nameof(items));
-
-            var args = Auxiliary.MergeArgs(key, items);
-
-            var result = await _db.ExecuteAsync(TOPK.ADD, args);
-            return (RedisResult[]?)result;
+            return (RedisResult[]?) await _db.ExecuteAsync(TopKCommandBuilder.Add(key, items));
         }
 
         /// <inheritdoc/>
         public long[] Count(RedisKey key, params RedisValue[] items)
         {
-            if (items.Length < 1)
-                throw new ArgumentOutOfRangeException(nameof(items));
-
-            var args = Auxiliary.MergeArgs(key, items);
-            return _db.Execute(TOPK.COUNT, args).ToLongArray();
+            return _db.Execute(TopKCommandBuilder.Count(key, items)).ToLongArray();
         }
 
         /// <inheritdoc/>
         public async Task<long[]> CountAsync(RedisKey key, params RedisValue[] items)
         {
-            if (items.Length < 1)
-                throw new ArgumentOutOfRangeException(nameof(items));
-
-            var args = Auxiliary.MergeArgs(key, items);
-            var result = await _db.ExecuteAsync(TOPK.COUNT, args);
-            return result.ToLongArray();
+            return (await _db.ExecuteAsync(TopKCommandBuilder.Count(key, items))).ToLongArray();
         }
 
 
         /// <inheritdoc/>
         public RedisResult[] IncrBy(RedisKey key, params Tuple<RedisValue, long>[] itemIncrements)
         {
-            if (itemIncrements.Length < 1)
-                throw new ArgumentOutOfRangeException(nameof(itemIncrements));
-
-            List<object> args = new List<object> { key };
-            foreach (var pair in itemIncrements)
-            {
-                args.Add(pair.Item1);
-                args.Add(pair.Item2);
-            }
-            return _db.Execute(TOPK.INCRBY, args).ToArray();
+            return _db.Execute(TopKCommandBuilder.IncrBy(key, itemIncrements)).ToArray();
         }
 
         /// <inheritdoc/>
         public async Task<RedisResult[]> IncrByAsync(RedisKey key, params Tuple<RedisValue, long>[] itemIncrements)
         {
-            if (itemIncrements.Length < 1)
-                throw new ArgumentOutOfRangeException(nameof(itemIncrements));
-
-            List<object> args = new List<object> { key };
-            foreach (var pair in itemIncrements)
-            {
-                args.Add(pair.Item1);
-                args.Add(pair.Item2);
-            }
-
-            var result = await _db.ExecuteAsync(TOPK.INCRBY, args);
-            return result.ToArray();
+            return (await _db.ExecuteAsync(TopKCommandBuilder.IncrBy(key, itemIncrements))).ToArray();
         }
 
         /// <inheritdoc/>
         public TopKInformation Info(RedisKey key)
         {
-            var info = _db.Execute(TOPK.INFO, key);
-            return info.ToTopKInfo();
+            return _db.Execute(TopKCommandBuilder.Info(key)).ToTopKInfo();
         }
 
         /// <inheritdoc/>
         public async Task<TopKInformation> InfoAsync(RedisKey key)
         {
-            var info = await _db.ExecuteAsync(TOPK.INFO, key);
-            return info.ToTopKInfo();
+            return (await _db.ExecuteAsync(TopKCommandBuilder.Info(key))).ToTopKInfo();
         }
 
         /// <inheritdoc/>
         public RedisResult[] List(RedisKey key, bool withcount = false)
         {
-            var result = (withcount) ? _db.Execute(TOPK.LIST, key, "WITHCOUNT")
-                                     : _db.Execute(TOPK.LIST, key);
-            return result.ToArray();
+            return _db.Execute(TopKCommandBuilder.List(key, withcount)).ToArray();
         }
 
         /// <inheritdoc/>
         public async Task<RedisResult[]> ListAsync(RedisKey key, bool withcount = false)
         {
-            var result = await((withcount) ? _db.ExecuteAsync(TOPK.LIST, key, "WITHCOUNT")
-                                            : _db.ExecuteAsync(TOPK.LIST, key));
-            return result.ToArray();
+            return (await _db.ExecuteAsync(TopKCommandBuilder.List(key, withcount))).ToArray();
         }
 
         /// <inheritdoc/>
         public bool Query(RedisKey key, RedisValue item)
         {
-            return _db.Execute(TOPK.QUERY, key, item).ToString() == "1";
+            return _db.Execute(TopKCommandBuilder.Query(key, item)).ToString() == "1";
         }
 
         /// <inheritdoc/>
         public async Task<bool> QueryAsync(RedisKey key, RedisValue item)
         {
-            var result = await _db.ExecuteAsync(TOPK.QUERY, key, item);
-            return result.ToString() == "1";
+            return (await _db.ExecuteAsync(TopKCommandBuilder.Query(key, item))).ToString() == "1";
         }
 
         /// <inheritdoc/>
         public bool[] Query(RedisKey key, params RedisValue[] items)
         {
-            if (items.Length < 1)
-                throw new ArgumentOutOfRangeException(nameof(items));
-
-            var args = Auxiliary.MergeArgs(key, items);
-
-            return _db.Execute(TOPK.QUERY, args).ToBooleanArray();
+            return _db.Execute(TopKCommandBuilder.Query(key, items)).ToBooleanArray();
         }
 
         /// <inheritdoc/>
         public async Task<bool[]> QueryAsync(RedisKey key, params RedisValue[] items)
         {
-            if (items.Length < 1)
-                throw new ArgumentOutOfRangeException(nameof(items));
-
-            var args = Auxiliary.MergeArgs(key, items);
-
-            var result = await _db.ExecuteAsync(TOPK.QUERY, args);
-            return result.ToBooleanArray();
+            return (await _db.ExecuteAsync(TopKCommandBuilder.Query(key, items))).ToBooleanArray();
         }
 
         /// <inheritdoc/>
         public bool Reserve(RedisKey key, long topk, long width = 7, long depth = 8, double decay = 0.9)
         {
-            return _db.Execute(TOPK.RESERVE, key, topk, width, depth, decay).OKtoBoolean();
+            return _db.Execute(TopKCommandBuilder.Reserve(key, topk, width, depth, decay)).OKtoBoolean();
         }
 
         /// <inheritdoc/>
         public async Task<bool> ReserveAsync(RedisKey key, long topk, long width = 7, long depth = 8, double decay = 0.9)
         {
-            var result = await _db.ExecuteAsync(TOPK.RESERVE, key, topk, width, depth, decay);
-            return result.OKtoBoolean();
+            return (await _db.ExecuteAsync(TopKCommandBuilder.Reserve(key, topk, width, depth, decay))).OKtoBoolean();
         }
     }
 }
