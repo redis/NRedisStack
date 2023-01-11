9766f27924fe2c33bf4b9d71a97b2d2d52a922d9
Shachar Pashchur <s.pashchur@gmail.com>
2023-01-11 17:27:43 +0200

    delete unnecessary code from TimeSeries

--- src/NRedisStack/TimeSeries/Extensions/BucketTimestampsExtensions.cs
+++ src/NRedisStack/TimeSeries/Extensions/BucketTimestampsExtensions.cs
@@ -12,14 +12,5 @@ namespace NRedisStack.Extensions
             TsBucketTimestamps.high => "+",
             _ => throw new ArgumentOutOfRangeException(nameof(bt), "Invalid TsBucketTimestamps type"),
         };
-
-        // TODO: check if this is needed:
-        // public static TsBucketTimestamps Asbt(string bt) => bt switch
-        // {
-        //     "-" => TsBucketTimestamps.low,
-        //     "~" => TsBucketTimestamps.mid,
-        //     "+" => TsBucketTimestamps.high,
-        //     _ => throw new ArgumentOutOfRangeException(nameof(bt), $"Invalid TsBucketTimestamps type '{bt}'"),
-        // };
     }
 }
