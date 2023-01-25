f9538beac43767f5476cdc7f578cc0c5c79f427b
shacharPash <shachar.pashchur@redis.com>
2023-01-25 17:56:23 +0200

    chagne to public static

--- src/NRedisStack/ModulPrefixes.cs
+++ src/NRedisStack/ModulPrefixes.cs
@@ -4,22 +4,22 @@ namespace NRedisStack.RedisStackCommands
 {
     public static class ModulPrefixes
     {
-        static public BloomCommands BF(this IDatabase db) => new BloomCommands(db);
+        public static BloomCommands BF(this IDatabase db) => new BloomCommands(db);
 
-        static public CuckooCommands CF(this IDatabase db) => new CuckooCommands(db);
+        public static CuckooCommands CF(this IDatabase db) => new CuckooCommands(db);
 
-        static public CmsCommands CMS(this IDatabase db) => new CmsCommands(db);
+        public static CmsCommands CMS(this IDatabase db) => new CmsCommands(db);
 
-        static public GraphCommands GRAPH(this IDatabase db) => new GraphCommands(db);
+        public static GraphCommands GRAPH(this IDatabase db) => new GraphCommands(db);
 
-        static public TopKCommands TOPK(this IDatabase db) => new TopKCommands(db);
+        public static TopKCommands TOPK(this IDatabase db) => new TopKCommands(db);
 
-        static public TdigestCommands TDIGEST(this IDatabase db) => new TdigestCommands(db);
+        public static TdigestCommands TDIGEST(this IDatabase db) => new TdigestCommands(db);
 
-        static public SearchCommands FT(this IDatabase db) => new SearchCommands(db);
+        public static SearchCommands FT(this IDatabase db) => new SearchCommands(db);
 
-        static public JsonCommands JSON(this IDatabase db) => new JsonCommands(db);
+        public static JsonCommands JSON(this IDatabase db) => new JsonCommands(db);
 
-        static public TimeSeriesCommands TS(this IDatabase db) => new TimeSeriesCommands(db);
+        public static TimeSeriesCommands TS(this IDatabase db) => new TimeSeriesCommands(db);
     }
 }
