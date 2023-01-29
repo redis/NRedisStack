98e44c1a04b157627962f5baeab29d0960264b05
shacharPash <shachar.pashchur@redis.com>
2023-01-29 10:56:15 +0200

    Add commands instance for each module in pipeline class

--- src/NRedisStack/Pipeline.cs
+++ src/NRedisStack/Pipeline.cs
@@ -16,7 +16,15 @@ public class Pipeline
         _batch.Execute();
     }
 
+    public IBloomCommandsAsync Bloom => new BloomCommandsAsync(_batch);
+    public ICmsCommandsAsync Cms => new CmsCommandsAsync(_batch);
+    public ICuckooCommandsAsync Cf => new CuckooCommandsAsync(_batch);
+    public IGraphCommandsAsync Graph => new GraphCommandsAsync(_batch);
     public IJsonCommandsAsync Json => new JsonCommandsAsync(_batch);
+    public ISearchCommandsAsync Search => new SearchCommandsAsync(_batch);
+    public ITdigestCommandsAsync Tdigest => new TdigestCommandsAsync(_batch);
+    public ITimeSeriesCommandsAsync Ts => new TimeSeriesCommandsAsync(_batch);
+    public ITopKCommandsAsync TopK => new TopKCommandsAsync(_batch);
 
     public IDatabaseAsync Db => _batch;
 }
