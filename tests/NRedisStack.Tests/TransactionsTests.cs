8a72c5c27390a8049efd3429774453509e23fb72
shacharPash <shachar.pashchur@redis.com>
2023-02-06 09:45:38 +0200

    add TestModulsTransaction

--- tests/NRedisStack.Tests/TransactionsTests.cs
+++ tests/NRedisStack.Tests/TransactionsTests.cs
@@ -1,4 +1,7 @@
 ﻿using Moq;
+using NRedisStack.RedisStackCommands;
+using NRedisStack.Search;
+using NRedisStack.Search.FT.CREATE;
 using StackExchange.Redis;
 using System.Text.Json;
 using Xunit;
@@ -32,66 +35,55 @@ namespace NRedisStack.Tests
             Assert.Equal("{\"Name\":\"Shachar\",\"Age\":23}", getResponse.Result.ToString());
         }
 
-        /*        [Fact]
-                public async Task TestCountMinSketchWithTopKInTransaction()
-                {
-                    IDatabase db = redisFixture.Redis.GetDatabase();
-                    db.Execute("FLUSHALL");
-                    var transaction = new Transactions(db);
+        [Fact]
+        public async Task TestModulsTransaction()
+        {
+            IDatabase db = redisFixture.Redis.GetDatabase();
+            db.Execute("FLUSHALL");
+            var tran = new Transactions(db);
 
-                    *//*transaction.TopK.ReserveAsync("aaa", 30, 2000, 7, 0.925);
-                    transaction.TopK.AddAsync("aaa", "foo", "bar");
-                    var results = transaction.TopK.ListAsync("aaa", true);
+            tran.Bf.ReserveAsync("bf-key", 0.001, 100);
+            tran.Bf.AddAsync("bf-key", "1");
+            tran.Cms.InitByDimAsync("cms-key", 100, 5);
+            tran.Cf.ReserveAsync("cf-key", 100);
+            tran.Graph.QueryAsync("graph-key", "CREATE ({name:'shachar',age:23})");
+            tran.Json.SetAsync("json-key", "$", "{}");
+            tran.Ft.CreateAsync("ft-key", new FTCreateParams(), new Schema().AddTextField("txt"));
+            tran.Tdigest.CreateAsync("tdigest-key", 100);
+            tran.Ts.CreateAsync("ts-key", 100);
+            tran.TopK.ReserveAsync("topk-key", 100, 100, 100);
 
+            Assert.False(db.KeyExists("bf-key"));
+            Assert.False(db.KeyExists("cms-key"));
+            Assert.False(db.KeyExists("cf-key"));
+            Assert.False(db.KeyExists("graph-key"));
+            Assert.False(db.KeyExists("json-key"));
+            Assert.Equal(0, db.FT()._List().Length);
+            Assert.False(db.KeyExists("tdigest-key"));
+            Assert.False(db.KeyExists("ts-key"));
+            Assert.False(db.KeyExists("topk-key"));
 
-                    transaction.Cms.InitByDimAsync("A", 1000, 5);
-                    var aValues = new Tuple<RedisValue, long>[3];
-                    aValues[0] = new Tuple<RedisValue, long>("foo", 5);
-                    aValues[1] = new Tuple<RedisValue, long>("bar", 3);
-                    aValues[2] = new Tuple<RedisValue, long>("baz", 9);
-                    transaction.Cms.IncrByAsync("A", aValues);
-                    transaction.Cms.QueryAsync("A", new RedisValue[] { "foo", "bar", "baz" });*//*
+            tran.Execute();
 
+            Assert.True(db.KeyExists("bf-key"));
+            Assert.True(db.KeyExists("cms-key"));
+            Assert.True(db.KeyExists("cf-key"));
+            Assert.True(db.KeyExists("graph-key"));
+            Assert.True(db.KeyExists("json-key"));
+            Assert.True(db.FT()._List().Length == 1);
+            Assert.True(db.KeyExists("tdigest-key"));
+            Assert.True(db.KeyExists("ts-key"));
+            Assert.True(db.KeyExists("topk-key"));
 
-                    transaction.Cms.InitByDimAsync("A", 1000, 5);
-                    transaction.Cms.InitByDimAsync("B", 1000, 5);
-                    transaction.Cms.InitByDimAsync("C", 1000, 5);
-
-                    var aValues = new Tuple<RedisValue, long>[3];
-                    aValues[0] = new Tuple<RedisValue, long>("foo", 5);
-                    aValues[1] = new Tuple<RedisValue, long>("bar", 3);
-                    aValues[2] = new Tuple<RedisValue, long>("baz", 9);
-
-                    transaction.Cms.IncrByAsync("A", aValues);
-
-                    var bValues = new Tuple<RedisValue, long>[3];
-                    bValues[0] = new Tuple<RedisValue, long>("foo", 2);
-                    bValues[1] = new Tuple<RedisValue, long>("bar", 3);
-                    bValues[2] = new Tuple<RedisValue, long>("baz", 1);
-
-                    transaction.Cms.IncrByAsync("B", bValues);
-
-                    var q1 = transaction.Cms.QueryAsync("A", new RedisValue[] { "foo", "bar", "baz" });
-                    Assert.Equal(new long[] { 5L, 3L, 9L }, q1);
-
-                    var q2 = transaction.Cms.QueryAsync("B", new RedisValue[] { "foo", "bar", "baz" });
-                    Assert.Equal(new long[] { 2L, 3L, 1L }, q2);
-
-                    transaction.Cms.MergeAsync("C", 2, new RedisValue[] { "A", "B" });
-
-                    var q3 = transaction.Cms.QueryAsync("C", new RedisValue[] { "foo", "bar", "baz" });
-                    Assert.Equal(new long[] { 7L, 6L, 10L }, q3);
-
-                    transaction.Cms.MergeAsync("C", 2, new RedisValue[] { "A", "B" }, new long[] { 1, 2 });
-
-                    var q4 = transaction.Cms.QueryAsync("C", new RedisValue[] { "foo", "bar", "baz" });
-                    Assert.Equal(new long[] { 9L, 9L, 11L }, q4);
-
-                    transaction.Cms.MergeAsync("C", 2, new RedisValue[] { "A", "B" }, new long[] { 2, 3 });
-
-
-                    var q5 = transaction.Cms.QueryAsync("C", new RedisValue[] { "foo", "bar", "baz" });
-                    Assert.Equal(new long[] { 16L, 15L, 21L }, (IEnumerable<long>)q5);
-                }*/
+            Assert.True(db.BF().Exists("bf-key", "1"));
+            Assert.True(db.CMS().Info("cms-key").Width == 100);
+            Assert.True(db.CF().Info("cf-key").Size > 0);
+            Assert.True(db.GRAPH().List().Count > 0);
+            Assert.False(db.JSON().Get("json-key").IsNull);
+            Assert.NotNull(db.FT().Info("ft-key"));
+            Assert.NotNull(db.TDIGEST().Info("tdigest-key"));
+            Assert.NotNull(db.TS().Info("ts-key"));
+            Assert.NotNull(db.TOPK().Info("topk-key"));
+        }
     }
 }
