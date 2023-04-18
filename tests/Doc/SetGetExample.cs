7f2fd59da8d15cf36a90d6a9622107968d2a1a00
David Maier <david.maier@redis.com>
2023-04-18 10:31:39 +0200

    Added a test project for documentation examples

---
+++ tests/Doc/SetGetExample.cs
@@ -0,0 +1,34 @@
+﻿// EXAMPLE: set_and_get
+// HIDE_START
+using System;
+using StackExchange.Redis;
+
+namespace NRedisStack.Doc;
+
+public class SetGetExample
+{
+    [Fact]
+    public void run()
+    {
+        var redis = ConnectionMultiplexer.Connect("localhost:6379");
+        var db = redis.GetDatabase();
+
+        //HIDE_END
+        bool status = db.StringSet("bike:1", "Process 134");
+            
+        if (status)
+            Console.WriteLine("Successfully added a bike.");
+
+        var value = db.StringGet("bike:1");
+
+        if (value.HasValue)
+            Console.WriteLine("The name of the bike is: " + value + ".");
+
+        //REMOVE_START
+        Assert.True(status);
+        Assert.Equal("Process 134", value.ToString());
+        //REMOVE_END
+    //HIDE_START
+    }
+}
+//HIDE_END
