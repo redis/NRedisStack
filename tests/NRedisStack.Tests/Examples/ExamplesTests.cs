ad11e0bb6df81af538579ad71d735e27325573ae
slorello89 <slorello89@gmail.com>
2023-05-01 09:24:04 -0400

    removing debugging

--- tests/NRedisStack.Tests/Examples/ExamplesTests.cs
+++ tests/NRedisStack.Tests/Examples/ExamplesTests.cs
@@ -323,7 +323,6 @@ public class ExaplesTests : AbstractNRedisStackTest, IDisposable
         var rsa = RSA.Create();
 
         var redisUserPrivateKeyText = File.ReadAllText(redisUserPrivateKeyPath).Trim();
-        testOutputHelper.WriteLine($"key length: {redisUserPrivateKeyText.Length} and starts with {redisUserPrivateKeyText.Substring(0,5)} and ends with {redisUserPrivateKeyText.Substring(redisUserPrivateKeyText.Length-5)}");
         rsa.ImportParameters(ImportPrivateKey(redisUserPrivateKeyText));
 
         var clientCert = redisUserCertificate.CopyWithPrivateKey(rsa);
