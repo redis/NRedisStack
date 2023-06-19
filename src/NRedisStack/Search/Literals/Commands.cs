059e1fa1e00a70144bc35319f01b54c192c77704
shacharPash <shachar.pashchur@redis.com>
2023-06-19 16:33:42 +0300

    delete unused literals

--- src/NRedisStack/Search/Literals/Commands.cs
+++ src/NRedisStack/Search/Literals/Commands.cs
@@ -9,13 +9,8 @@
         public const string ALIASUPDATE = "FT.ALIASUPDATE";
         public const string ALTER = "FT.ALTER";
         public const string CONFIG = "FT.CONFIG";
-        // public const string CONFIG_GET = "FT.CONFIG GET";
-        // public const string CONFIG_HELP = "FT.CONFIG HELP";
-        // public const string CONFIG_SET = "FT.CONFIG SET";
         public const string CREATE = "FT.CREATE";
         public const string CURSOR = "FT.CURSOR";
-        // public const string CURSOR_DEL = "FT.CURSOR DEL";
-        // public const string CURSOR_READ = "FT.CURSOR READ";
         public const string DICTADD = "FT.DICTADD";
         public const string DICTDEL = "FT.DICTDEL";
         public const string DICTDUMP = "FT.DICTDUMP";
