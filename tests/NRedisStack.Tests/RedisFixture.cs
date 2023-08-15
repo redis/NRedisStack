eeb2edf7204e2a19ab556af4557ab65db5e0d66d
shacharPash <shachar.pashchur@redis.com>
2023-08-15 17:08:19 +0300

    add opthin to connect cluster with dotnet test

--- tests/NRedisStack.Tests/RedisFixture.cs
+++ tests/NRedisStack.Tests/RedisFixture.cs
@@ -1,14 +1,47 @@
+using System.Net;
+using Org.BouncyCastle.Tls;
 using StackExchange.Redis;
 
 namespace NRedisStack.Tests
 {
     public class RedisFixture : IDisposable
     {
-
-
         // Set the enviroment variable to specify your own alternet host and port:
-        string redis = Environment.GetEnvironmentVariable("REDIS") ?? "localhost:6379";
-        public RedisFixture() => Redis = ConnectionMultiplexer.Connect($"{redis}");
+        string redisStandalone = Environment.GetEnvironmentVariable("REDIS") ?? "localhost:6379";
+        string? redisCluster = Environment.GetEnvironmentVariable("REDIS_CLUSTER");
+        string? numRedisClusterNodesEnv = Environment.GetEnvironmentVariable("NUM_REDIS_CLUSTER_NODES");
+        // bool redisClusterMode = false; // TODO: check if this is needed
+
+        public RedisFixture()
+        {
+            // Redis Cluster
+            if (redisCluster != null && numRedisClusterNodesEnv != null)
+            {
+                // Split to host and port
+                string[] parts = redisCluster.Split(':');
+                string host = parts[0];
+                int startPort = int.Parse(parts[1]);
+
+                var endpoints = new EndPointCollection();
+                int numRedisClusterNodes = int.Parse(numRedisClusterNodesEnv);
+                for (int i = 0; i < numRedisClusterNodes; i++)
+                {
+                    endpoints.Add(host, startPort + i);
+                }
+
+
+                ConfigurationOptions clusterConfig = new ConfigurationOptions
+                {
+                    EndPoints = endpoints
+                };
+                // redisClusterMode = true;
+                Redis = ConnectionMultiplexer.Connect(clusterConfig);
+            }
+
+            // Redis Standalone
+            else
+                Redis = ConnectionMultiplexer.Connect($"{redisStandalone}");
+        }
 
         public void Dispose()
         {
