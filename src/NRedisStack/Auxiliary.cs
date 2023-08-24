6e82630bd413b648cce4359aeceeb1f5fa0309c6
shacharPash <shachar.pashchur@redis.com>
2023-08-24 14:34:38 +0300

    Execute broadcast

--- src/NRedisStack/Auxiliary.cs
+++ src/NRedisStack/Auxiliary.cs
@@ -35,5 +35,53 @@ namespace NRedisStack
         {
             return await db.ExecuteAsync(command.Command, command.Args);
         }
+
+        public static List<RedisResult> ExecuteBroadcast(this IDatabaseAsync db, string command)
+                => db.ExecuteBroadcast(new SerializedCommand(command));
+
+        public static List<RedisResult> ExecuteBroadcast(this IDatabaseAsync db, SerializedCommand command)
+        {
+            var redis = db.Multiplexer;
+            var endpoints = redis.GetEndPoints();
+            var results = new List<RedisResult>(endpoints.Length);
+
+            foreach (var endPoint in endpoints)
+            {
+                var server = redis.GetServer(endPoint);
+
+                if (server.IsReplica)
+                {
+                    continue; // Skip replica nodes
+                }
+                // Send your command to the master node
+
+                results.Add(server.Multiplexer.GetDatabase().Execute(command));
+            }
+            return results;
+        }
+
+        public async static Task<List<RedisResult>> ExecuteBroadcastAsync(this IDatabaseAsync db, string command)
+                => await db.ExecuteBroadcastAsync(new SerializedCommand(command));
+
+        public async static Task<List<RedisResult>> ExecuteBroadcastAsync(this IDatabaseAsync db, SerializedCommand command)
+        {
+            var redis = db.Multiplexer;
+            var endpoints = redis.GetEndPoints();
+            var results = new List<RedisResult>(endpoints.Length);
+
+            foreach (var endPoint in endpoints)
+            {
+                var server = redis.GetServer(endPoint);
+
+                if (server.IsReplica)
+                {
+                    continue; // Skip replica nodes
+                }
+                // Send your command to the master node
+
+                results.Add(await server.Multiplexer.GetDatabase().ExecuteAsync(command));
+            }
+            return results;
+        }
     }
 }
