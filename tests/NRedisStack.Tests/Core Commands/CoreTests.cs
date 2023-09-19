3409bfece12842ca1017b0631a80eab05de186b5
shacharPash <shachar.pashchur@redis.com>
2023-09-19 17:43:58 +0300

    change test

--- tests/NRedisStack.Tests/Core Commands/CoreTests.cs
+++ tests/NRedisStack.Tests/Core Commands/CoreTests.cs
@@ -5,6 +5,7 @@ using static NRedisStack.Auxiliary;
 using StackExchange.Redis;
 using System.Xml.Linq;
 using System.Reflection;
+using NRedisStack.RedisStackCommands;
 
 
 namespace NRedisStack.Tests.Core;
@@ -14,35 +15,42 @@ public class CoreTests : AbstractNRedisStackTest, IDisposable
     private readonly string key = "CORE_TESTS";
     public CoreTests(RedisFixture redisFixture) : base(redisFixture) { }
 
+    [SkipIfRedis(Comparison.LessThan, "7.1.242")]
+    public void TestSetInfoDefaultValue()
+    {
+        var redis = ConnectionMultiplexer.Connect("localhost");
+        var db = redis.GetDatabase();
+
+        db.Execute("FLUSHALL");
+        db.Execute(new SerializedCommand("PING")); // only the extension method of Execute (which is used for all the commands of Redis Stack) will set the library name and version.
+
+        var info = db.Execute("CLIENT", "INFO").ToString();
+        Assert.EndsWith($"lib-name=NRedisStack;.NET-{Environment.Version} lib-ver={GetNRedisStackVersion()}\n", info);
+    }
+
+    [SkipIfRedis(Comparison.LessThan, "7.1.242")]
+    public void TestSetInfoWithValue()
+    {
+        var redis = ConnectionMultiplexer.Connect("localhost");
+        var db = redis.GetDatabase("MyLibraryName;v1.0.0");
+
+        db.Execute("FLUSHALL");
+        db.Execute(new SerializedCommand("PING")); // only the extension method of Execute (which is used for all the commands of Redis Stack) will set the library name and version.
+
+        var info = db.Execute("CLIENT", "INFO").ToString();
+        Assert.EndsWith($"NRedisStack(MyLibraryName;v1.0.0);.NET-{Environment.Version}) lib-ver={GetNRedisStackVersion()}\n", info);
+    }
+
     // [SkipIfRedis(Comparison.LessThan, "7.1.242")]
-    // public void TestSetInfo()
+    // public void TestSetInfoNull()
     // {
     //     var redis = ConnectionMultiplexer.Connect("localhost");
-    //     var db = redis.GetDatabase("name"); // TODO: find a way that the user will not need to pass the library name and version if he wants to use the default values.
+    //     var db = redis.GetDatabase(null);
 
     //     db.Execute("FLUSHALL");
+    //     db.Execute(new SerializedCommand("PING")); // only the extension method of Execute (which is used for all the commands of Redis Stack) will set the library name and version.
+
     //     var info = db.Execute("CLIENT", "INFO").ToString();
-    //     Assert.EndsWith($"lib-name=NRedisStack(SE.Redis-v{GetStackExchangeRedisVersion()};.NET-{Environment.Version}) lib-ver={GetNRedisStackVersion()}\n", info);
-
-    //     Assert.True(db.ClientSetInfo(SetInfoAttr.LibraryName, "anylibname"));
-    //     Assert.True(db.ClientSetInfo(SetInfoAttr.LibraryVersion, "1.2.3"));
-    //     info = db.Execute("CLIENT", "INFO").ToString();
-    //     Assert.EndsWith("lib-name=anylibname lib-ver=1.2.3\n", info);
-    // }
-
-    // [SkipIfRedis(Comparison.LessThan, "7.1.242")]
-    // public async Task TestSetInfoAsync()
-    // {
-    //     var redis = ConnectionMultiplexer.Connect("localhost");
-
-    //     var db = redis.GetDatabase(""); // TODO: find a way that the user will not need to pass the library name and version if he wants to use the default values.
-    //     db.Execute("FLUSHALL");
-    //     var info = (await db.ExecuteAsync("CLIENT", "INFO")).ToString();
-    //     Assert.EndsWith($"lib-name=NRedisStack(SE.Redis-v{GetStackExchangeRedisVersion()};.NET-{Environment.Version}) lib-ver={GetNRedisStackVersion()}\n", info);
-
-    //     Assert.True(await db.ClientSetInfoAsync(SetInfoAttr.LibraryName, "anylibname"));
-    //     Assert.True(await db.ClientSetInfoAsync(SetInfoAttr.LibraryVersion, "1.2.3"));
-    //     info = (await db.ExecuteAsync("CLIENT", "INFO")).ToString();
-    //     Assert.EndsWith("lib-name=anylibname lib-ver=1.2.3\n", info);
+    //     Assert.EndsWith($"lib-name= lib-ver=\n", info);
     // }
 }
