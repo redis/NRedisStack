39b228c15b54f0c38aabb4cbf1f1bc1ab4a89ed2
shacharPash <shachar.pashchur@redis.com>
2023-10-22 13:15:10 +0300

    dotnet format

--- tests/Doc/StringSnippets.cs
+++ tests/Doc/StringSnippets.cs
@@ -18,25 +18,25 @@ public class StringSnippets
     {
         var redis = ConnectionMultiplexer.Connect("localhost:6379");
         var db = redis.GetDatabase();
-        
+
         //HIDE_END
 
         //REMOVE_START
-        db.KeyDelete(new RedisKey[] {"bike:1", "bike:2", "bike:3", "total_crashes"});
+        db.KeyDelete(new RedisKey[] { "bike:1", "bike:2", "bike:3", "total_crashes" });
         //REMOVE_END
-        
+
         // STEP_START set_get
         var res1 = db.StringSet("bike:1", "Deimos");
         Console.WriteLine(res1); // true
         var res2 = db.StringGet("bike:1");
         Console.WriteLine(res2); // Deimos
         // STEP_END
-        
+
         //REMOVE_START
         Assert.True(res1);
         Assert.Equal("Deimos", res2);
         //REMOVE_END
-        
+
         //STEP_START setnx_xx
         var res3 = db.StringSet("bike:1", "bike", when: When.NotExists);
         Console.WriteLine(res3); // false
@@ -44,12 +44,12 @@ public class StringSnippets
         var res4 = db.StringSet("bike:1", "bike", when: When.Exists);
         Console.WriteLine(res4); // true
         //STEP_END
-        
+
         //REMOVE_START
         Assert.False(res3);
         Assert.True(res4);
         //REMOVE_END
-        
+
         //STEP_START mset
         var res5 = db.StringSet(new KeyValuePair<RedisKey, RedisValue>[]
         {
@@ -59,12 +59,12 @@ public class StringSnippets
         var res6 = db.StringGet(new RedisKey[] { "bike:1", "bike:2", "bike:3" });
         Console.WriteLine(res6);
         //STEP_END
-        
+
         //REMOVE_START
         Assert.True(res5);
-        Assert.Equal(new[]{"Deimos", "Ares", "Vanth"}, res6.Select(x=>x.ToString()).ToArray());
+        Assert.Equal(new[] { "Deimos", "Ares", "Vanth" }, res6.Select(x => x.ToString()).ToArray());
         //REMOVE_END
-        
+
         //STEP_START incr
         db.StringSet("total_crashes", 0);
         var res7 = db.StringIncrement("total_crashes");
@@ -72,7 +72,7 @@ public class StringSnippets
         var res8 = db.StringIncrement("total_crashes", 10);
         Console.WriteLine(res8);
         //STEP_END
-        
+
         //REMOVE_START
         Assert.Equal(1, res7);
         Assert.Equal(11, res8);
