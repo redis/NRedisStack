a54aff8b56591d1bb728f323925f7e590e0a1143
shacharPash <shachar.pashchur@redis.com>
2023-12-11 11:08:56 +0200

    import dll test with no path

---
+++ tests/NRedisStack.Tests/ImportDllTest.cs
@@ -0,0 +1,25 @@
+using Xunit;
+using System.Runtime.InteropServices;
+
+namespace NRedisStack.Tests
+{
+    public class ImportDllTest : AbstractNRedisStackTest, IDisposable
+    {
+        const string LibName = "libcommands.dylib";
+
+        [DllImport(LibName, EntryPoint = "numbers", CallingConvention = CallingConvention.Cdecl)]
+        private static extern int numbers(int a, int b);
+
+        [DllImport(LibName, EntryPoint = "hello_world", CallingConvention = CallingConvention.Cdecl)]
+        private static extern string hello_world();
+
+        public ImportDllTest(RedisFixture redisFixture) : base(redisFixture) { }
+
+        [Fact]
+        public void TestImportDll()
+        {
+            Assert.Equal(3, numbers(1, 2));
+            Assert.Equal("Hello, world", hello_world());
+        }
+    }
+}
