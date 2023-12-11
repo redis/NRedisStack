962b456b15c7610b1353689ded5052d4378f6e4a
shacharPash <shachar.pashchur@redis.com>
2023-12-11 14:44:37 +0200

    add .so

--- tests/NRedisStack.Tests/ImportDllTest.cs
+++ tests/NRedisStack.Tests/ImportDllTest.cs
@@ -5,7 +5,7 @@ namespace NRedisStack.Tests
 {
     public class ImportDllTest : AbstractNRedisStackTest, IDisposable
     {
-        const string LibName = "libcommands.dylib";
+        const string LibName = "libcommands";
 
         [DllImport(LibName, EntryPoint = "numbers")]
         private static extern int numbers(int a, int b);
