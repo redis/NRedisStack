8948d82062a62a9f32d3ec3ef665ed0bdff77dbb
Gabriel Erzse <gabriel.erzse@redis.com>
2024-01-25 11:46:41 +0200

    Add support for BZMPOP, BZPOPMIN and BZPOPMAX
    Add support for the BZMPOP, BZPOPMIN, BZPOPMAX commands.
    
    Issues #232, #233 and #234.
    
    These commands are blocking on the server, so they go against the current
    policy of the StackExchange.Redis library. Therefore make it obvious in
    the code documentation that attention must be given to the timeout in
    the connection multiplexer, client-side.
    
    The StackExchange.Redis library already defines a type for the payload
    returned by BZMPOP (which is the same as for ZMPOP), namely the
    SortedSetPopResult class. However, the constructor of that class is
    internal in the library, so we can't create instances of it. Therefore
    roll our out type for a <value, score> pair, and use Tuple to pair a key
    with a list of such <value, score> pairs.
    
    Instead of using Order to signal from which end of the sorted set to
    pop, define a MinMaxModifier enum, which more clearly expresses the
    intention and maps directly to the Redis command being executed.

--- tests/NRedisStack.Tests/Core Commands/CoreTests.cs
+++ tests/NRedisStack.Tests/Core Commands/CoreTests.cs
@@ -1,10 +1,8 @@
 using Xunit;
 using NRedisStack.Core;
-using NRedisStack;
 using static NRedisStack.Auxiliary;
 using StackExchange.Redis;
-using System.Xml.Linq;
-using System.Reflection;
+using NRedisStack.Core.DataTypes;
 using NRedisStack.RedisStackCommands;
 
 
@@ -145,5 +143,275 @@ public class CoreTests : AbstractNRedisStackTest, IDisposable
 
         // Assert that the extracted sub-strings are equal
         Assert.Equal(infoAfterLibNameToEnd, infoBeforeLibNameToEnd);
+    }
+
+    [SkipIfRedis(Is.OSSCluster, Comparison.LessThan, "7.0.0")]
+    public void TestBzmPop()
+    {
+        var redis = ConnectionMultiplexer.Connect("localhost");
+
+        var db = redis.GetDatabase(null);
+        db.Execute("FLUSHALL");
+
+        var sortedSetKey = "my-set";
+
+        db.SortedSetAdd(sortedSetKey, "a", 1.5);
+        db.SortedSetAdd(sortedSetKey, "b", 5.1);
+        db.SortedSetAdd(sortedSetKey, "c", 3.7);
+        db.SortedSetAdd(sortedSetKey, "d", 9.4);
+        db.SortedSetAdd(sortedSetKey, "e", 7.76);
+
+        // Pop two items with default order, which means it will pop the minimum values.
+        var resultWithDefaultOrder = db.BzmPop(0, sortedSetKey, MinMaxModifier.Min, 2);
+
+        Assert.NotNull(resultWithDefaultOrder);
+        Assert.Equal(sortedSetKey, resultWithDefaultOrder!.Item1);
+        Assert.Equal(2, resultWithDefaultOrder.Item2.Count);
+        Assert.Equal("a", resultWithDefaultOrder.Item2[0].Value.ToString());
+        Assert.Equal("c", resultWithDefaultOrder.Item2[1].Value.ToString());
+
+        // Pop one more item, with descending order, which means it will pop the maximum value.
+        var resultWithDescendingOrder = db.BzmPop(0, sortedSetKey, MinMaxModifier.Max, 1);
+
+        Assert.NotNull(resultWithDescendingOrder);
+        Assert.Equal(sortedSetKey, resultWithDescendingOrder!.Item1);
+        Assert.Single(resultWithDescendingOrder.Item2);
+        Assert.Equal("d", resultWithDescendingOrder.Item2[0].Value.ToString());
+    }
+
+    [SkipIfRedis(Is.OSSCluster, Comparison.LessThan, "7.0.0")]
+    public void TestBzmPopNull()
+    {
+        var redis = ConnectionMultiplexer.Connect("localhost");
+
+        var db = redis.GetDatabase(null);
+        db.Execute("FLUSHALL");
+
+        // Nothing in the set, and a short server timeout, which yields null.
+        var result = db.BzmPop(0.5, "my-set", MinMaxModifier.Min, null);
+
+        Assert.Null(result);
+    }
+
+    [SkipIfRedis(Is.OSSCluster, Comparison.LessThan, "7.0.0")]
+    public void TestBzmPopMultiplexerTimeout()
+    {
+        var configurationOptions = new ConfigurationOptions();
+        configurationOptions.SyncTimeout = 1000;
+        configurationOptions.EndPoints.Add("localhost");
+        var redis = ConnectionMultiplexer.Connect(configurationOptions);
+
+        var db = redis.GetDatabase(null);
+        db.Execute("FLUSHALL");
+
+        // Server would wait forever, but the multiplexer times out in 1 second.
+        Assert.Throws<RedisTimeoutException>(() => db.BzmPop(0, "my-set", MinMaxModifier.Min));
+    }
+
+    [SkipIfRedis(Is.OSSCluster, Comparison.LessThan, "7.0.0")]
+    public void TestBzmPopMultipleSets()
+    {
+        var redis = ConnectionMultiplexer.Connect("localhost");
+
+        var db = redis.GetDatabase(null);
+        db.Execute("FLUSHALL");
+
+        db.SortedSetAdd("set-one", "a", 1.5);
+        db.SortedSetAdd("set-one", "b", 5.1);
+        db.SortedSetAdd("set-one", "c", 3.7);
+        db.SortedSetAdd("set-two", "d", 9.4);
+        db.SortedSetAdd("set-two", "e", 7.76);
+
+        var result = db.BzmPop(0, "set-two", MinMaxModifier.Max);
+
+        Assert.NotNull(result);
+        Assert.Equal("set-two", result!.Item1);
+        Assert.Single(result.Item2);
+        Assert.Equal("d", result.Item2[0].Value.ToString());
+
+        result = db.BzmPop(0, new[] { new RedisKey("set-two"), new RedisKey("set-one") }, MinMaxModifier.Min);
+
+        Assert.NotNull(result);
+        Assert.Equal("set-two", result!.Item1);
+        Assert.Single(result.Item2);
+        Assert.Equal("e", result.Item2[0].Value.ToString());
+
+        result = db.BzmPop(0, new[] { new RedisKey("set-two"), new RedisKey("set-one") }, MinMaxModifier.Max);
+
+        Assert.NotNull(result);
+        Assert.Equal("set-one", result!.Item1);
+        Assert.Single(result.Item2);
+        Assert.Equal("b", result.Item2[0].Value.ToString());
+
+        result = db.BzmPop(0, "set-one", MinMaxModifier.Min, count: 2);
+
+        Assert.NotNull(result);
+        Assert.Equal("set-one", result!.Item1);
+        Assert.Equal(2, result.Item2.Count);
+        Assert.Equal("a", result.Item2[0].Value.ToString());
+        Assert.Equal("c", result.Item2[1].Value.ToString());
+    }
+
+    [SkipIfRedis(Is.OSSCluster, Comparison.LessThan, "7.0.0")]
+    public void TestBzmPopNoKeysProvided()
+    {
+        var redis = ConnectionMultiplexer.Connect("localhost");
+
+        var db = redis.GetDatabase(null);
+        db.Execute("FLUSHALL");
+
+        // Server would wait forever, but the multiplexer times out in 1 second.
+        Assert.Throws<ArgumentException>(() => db.BzmPop(0, Array.Empty<RedisKey>(), MinMaxModifier.Min));
+    }
+
+    [SkipIfRedis(Is.OSSCluster, Comparison.LessThan, "7.0.0")]
+    public void TestBzmPopWithOrderEnum()
+    {
+        var redis = ConnectionMultiplexer.Connect("localhost");
+
+        var db = redis.GetDatabase(null);
+        db.Execute("FLUSHALL");
+
+        var sortedSetKey = "my-set";
+
+        db.SortedSetAdd(sortedSetKey, "a", 1.5);
+        db.SortedSetAdd(sortedSetKey, "b", 5.1);
+        db.SortedSetAdd(sortedSetKey, "c", 3.7);
+
+        // Pop two items with default order, which means it will pop the minimum values.
+        var resultWithDefaultOrder = db.BzmPop(0, sortedSetKey, Order.Ascending.ToMinMax());
+
+        Assert.NotNull(resultWithDefaultOrder);
+        Assert.Equal(sortedSetKey, resultWithDefaultOrder!.Item1);
+        Assert.Single(resultWithDefaultOrder.Item2);
+        Assert.Equal("a", resultWithDefaultOrder.Item2[0].Value.ToString());
+
+        // Pop one more item, with descending order, which means it will pop the maximum value.
+        var resultWithDescendingOrder = db.BzmPop(0, sortedSetKey, Order.Descending.ToMinMax());
+
+        Assert.NotNull(resultWithDescendingOrder);
+        Assert.Equal(sortedSetKey, resultWithDescendingOrder!.Item1);
+        Assert.Single(resultWithDescendingOrder.Item2);
+        Assert.Equal("b", resultWithDescendingOrder.Item2[0].Value.ToString());
+    }
+
+    [SkipIfRedis(Is.OSSCluster, Comparison.LessThan, "5.0.0")]
+    public void TestBzPopMin()
+    {
+        var redis = ConnectionMultiplexer.Connect("localhost");
+
+        var db = redis.GetDatabase(null);
+        db.Execute("FLUSHALL");
+
+        var sortedSetKey = "my-set";
+
+        db.SortedSetAdd(sortedSetKey, "a", 1.5);
+        db.SortedSetAdd(sortedSetKey, "b", 5.1);
+
+        var result = db.BzPopMin(sortedSetKey, 0);
+
+        Assert.NotNull(result);
+        Assert.Equal(sortedSetKey, result!.Item1);
+        Assert.Equal("a", result.Item2.Value.ToString());
+        Assert.Equal(1.5, result.Item2.Score);
+    }
+
+    [SkipIfRedis(Is.OSSCluster, Comparison.LessThan, "5.0.0")]
+    public void TestBzPopMinNull()
+    {
+        var redis = ConnectionMultiplexer.Connect("localhost");
+
+        var db = redis.GetDatabase(null);
+        db.Execute("FLUSHALL");
+
+        // Nothing in the set, and a short server timeout, which yields null.
+        var result = db.BzPopMin("my-set", 0.5);
+
+        Assert.Null(result);
+    }
+
+    [SkipIfRedis(Is.OSSCluster, Comparison.LessThan, "5.0.0")]
+    public void TestBzPopMinMultipleSets()
+    {
+        var redis = ConnectionMultiplexer.Connect("localhost");
+
+        var db = redis.GetDatabase(null);
+        db.Execute("FLUSHALL");
+
+        db.SortedSetAdd("set-one", "a", 1.5);
+        db.SortedSetAdd("set-one", "b", 5.1);
+        db.SortedSetAdd("set-two", "e", 7.76);
+
+        var result = db.BzPopMin(new[] { new RedisKey("set-two"), new RedisKey("set-one") }, 0);
+
+        Assert.NotNull(result);
+        Assert.Equal("set-two", result!.Item1);
+        Assert.Equal("e", result.Item2.Value.ToString());
+
+        result = db.BzPopMin(new[] { new RedisKey("set-two"), new RedisKey("set-one") }, 0);
+
+        Assert.NotNull(result);
+        Assert.Equal("set-one", result!.Item1);
+        Assert.Equal("a", result.Item2.Value.ToString());
+    }
+
+    [SkipIfRedis(Is.OSSCluster, Comparison.LessThan, "5.0.0")]
+    public void TestBzPopMax()
+    {
+        var redis = ConnectionMultiplexer.Connect("localhost");
+
+        var db = redis.GetDatabase(null);
+        db.Execute("FLUSHALL");
+
+        var sortedSetKey = "my-set";
+
+        db.SortedSetAdd(sortedSetKey, "a", 1.5);
+        db.SortedSetAdd(sortedSetKey, "b", 5.1);
+
+        var result = db.BzPopMax(sortedSetKey, 0);
+
+        Assert.NotNull(result);
+        Assert.Equal(sortedSetKey, result!.Item1);
+        Assert.Equal("b", result.Item2.Value.ToString());
+        Assert.Equal(5.1, result.Item2.Score);
+    }
+
+    [SkipIfRedis(Is.OSSCluster, Comparison.LessThan, "5.0.0")]
+    public void TestBzPopMaxNull()
+    {
+        var redis = ConnectionMultiplexer.Connect("localhost");
+
+        var db = redis.GetDatabase(null);
+        db.Execute("FLUSHALL");
+
+        // Nothing in the set, and a short server timeout, which yields null.
+        var result = db.BzPopMax("my-set", 0.5);
+
+        Assert.Null(result);
+    }
+
+    [SkipIfRedis(Is.OSSCluster, Comparison.LessThan, "5.0.0")]
+    public void TestBzPopMaxMultipleSets()
+    {
+        var redis = ConnectionMultiplexer.Connect("localhost");
+
+        var db = redis.GetDatabase(null);
+        db.Execute("FLUSHALL");
+
+        db.SortedSetAdd("set-one", "a", 1.5);
+        db.SortedSetAdd("set-one", "b", 5.1);
+        db.SortedSetAdd("set-two", "e", 7.76);
+
+        var result = db.BzPopMax(new[] { new RedisKey("set-two"), new RedisKey("set-one") }, 0);
+
+        Assert.NotNull(result);
+        Assert.Equal("set-two", result!.Item1);
+        Assert.Equal("e", result.Item2.Value.ToString());
+
+        result = db.BzPopMax(new[] { new RedisKey("set-two"), new RedisKey("set-one") }, 0);
+
+        Assert.NotNull(result);
+        Assert.Equal("set-one", result!.Item1);
+        Assert.Equal("b", result.Item2.Value.ToString());
     }
 }
