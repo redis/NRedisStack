24ad2aa8cf9b2c6df11d8411209ddf0e4eb41372
nimanikoo <nikoonazar.nima@gmail.com>
2024-01-26 20:30:09 +0330

    use primary constructor in CmsCommands.cs

--- src/NRedisStack/CountMinSketch/CmsCommands.cs
+++ src/NRedisStack/CountMinSketch/CmsCommands.cs
@@ -1,57 +1,49 @@
 using NRedisStack.CountMinSketch.DataTypes;
 using StackExchange.Redis;
-namespace NRedisStack
+namespace NRedisStack;
+
+public class CmsCommands(IDatabase db) : CmsCommandsAsync(db), ICmsCommands
 {
-
-    public class CmsCommands : CmsCommandsAsync, ICmsCommands
+    /// <inheritdoc/>
+    public long IncrBy(RedisKey key, RedisValue item, long increment)
     {
-        IDatabase _db;
-        public CmsCommands(IDatabase db) : base(db)
-        {
-            _db = db;
-        }
+        return db.Execute(CmsCommandBuilder.IncrBy(key, item, increment)).ToLong();
+    }
 
-        /// <inheritdoc/>
-        public long IncrBy(RedisKey key, RedisValue item, long increment)
-        {
-            return _db.Execute(CmsCommandBuilder.IncrBy(key, item, increment)).ToLong();
-        }
+    /// <inheritdoc/>
+    public long[] IncrBy(RedisKey key, Tuple<RedisValue, long>[] itemIncrements)
+    {
+        return db.Execute(CmsCommandBuilder.IncrBy(key, itemIncrements)).ToLongArray();
+    }
 
-        /// <inheritdoc/>
-        public long[] IncrBy(RedisKey key, Tuple<RedisValue, long>[] itemIncrements)
-        {
-            return _db.Execute(CmsCommandBuilder.IncrBy(key, itemIncrements)).ToLongArray();
-        }
+    /// <inheritdoc/>
+    public CmsInformation Info(RedisKey key)
+    {
+        var info = db.Execute(CmsCommandBuilder.Info(key));
+        return info.ToCmsInfo();
+    }
 
-        /// <inheritdoc/>
-        public CmsInformation Info(RedisKey key)
-        {
-            var info = _db.Execute(CmsCommandBuilder.Info(key));
-            return info.ToCmsInfo();
-        }
+    /// <inheritdoc/>
+    public bool InitByDim(RedisKey key, long width, long depth)
+    {
+        return db.Execute(CmsCommandBuilder.InitByDim(key, width, depth)).OKtoBoolean();
+    }
 
-        /// <inheritdoc/>
-        public bool InitByDim(RedisKey key, long width, long depth)
-        {
-            return _db.Execute(CmsCommandBuilder.InitByDim(key, width, depth)).OKtoBoolean();
-        }
+    /// <inheritdoc/>
+    public bool InitByProb(RedisKey key, double error, double probability)
+    {
+        return db.Execute(CmsCommandBuilder.InitByProb(key, error, probability)).OKtoBoolean();
+    }
 
-        /// <inheritdoc/>
-        public bool InitByProb(RedisKey key, double error, double probability)
-        {
-            return _db.Execute(CmsCommandBuilder.InitByProb(key, error, probability)).OKtoBoolean();
-        }
+    /// <inheritdoc/>
+    public bool Merge(RedisValue destination, long numKeys, RedisValue[] source, long[]? weight = null)
+    {
+        return db.Execute(CmsCommandBuilder.Merge(destination, numKeys, source, weight)).OKtoBoolean();
+    }
 
-        /// <inheritdoc/>
-        public bool Merge(RedisValue destination, long numKeys, RedisValue[] source, long[]? weight = null)
-        {
-            return _db.Execute(CmsCommandBuilder.Merge(destination, numKeys, source, weight)).OKtoBoolean();
-        }
-
-        /// <inheritdoc/>
-        public long[] Query(RedisKey key, params RedisValue[] items)
-        {
-            return _db.Execute(CmsCommandBuilder.Query(key, items)).ToLongArray();
-        }
+    /// <inheritdoc/>
+    public long[] Query(RedisKey key, params RedisValue[] items)
+    {
+        return db.Execute(CmsCommandBuilder.Query(key, items)).ToLongArray();
     }
 }
