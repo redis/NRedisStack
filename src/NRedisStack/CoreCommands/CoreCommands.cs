5ffe8ad9a156243358b23b066e3609c500fa269c
nimanikoo <nikoonazar.nima@gmail.com>
2024-01-26 20:16:05 +0330

    Add Db to xml summary doc

--- src/NRedisStack/CoreCommands/CoreCommands.cs
+++ src/NRedisStack/CoreCommands/CoreCommands.cs
@@ -1,23 +1,22 @@
 using NRedisStack.Core;
 using StackExchange.Redis;
-namespace NRedisStack
-{
+namespace NRedisStack;
 
-    public static class CoreCommands
+public static class CoreCommands
+{
+    /// <summary>
+    /// Sets information specific to the client or connection.
+    /// </summary>
+    /// <param name="db"></param>
+    /// <param name="attr">which attribute to set</param>
+    /// <param name="value">the attribute value</param>
+    /// <returns><see langword="true"/> if the attribute name was successfully set, Error otherwise.</returns>
+    /// <remarks><seealso href="https://redis.io/commands/client-setinfo/"/></remarks>
+    public static bool ClientSetInfo(this IDatabase db, SetInfoAttr attr, string value)
     {
-        /// <summary>
-        /// Sets information specific to the client or connection.
-        /// </summary>
-        /// <param name="attr">which attribute to set</param>
-        /// <param name="value">the attribute value</param>
-        /// <returns><see langword="true"/> if the attribute name was successfully set, Error otherwise.</returns>
-        /// <remarks><seealso href="https://redis.io/commands/client-setinfo/"/></remarks>
-        public static bool ClientSetInfo(this IDatabase db, SetInfoAttr attr, string value)
-        {
-            var compareVersions = db.Multiplexer.GetServer(db.Multiplexer.GetEndPoints()[0]).Version.CompareTo(new Version(7, 1, 242));
-            if (compareVersions < 0) // the server does not support the CLIENT SETNAME command
-                return false;
-            return db.Execute(CoreCommandBuilder.ClientSetInfo(attr, value)).OKtoBoolean();
-        }
+        var compareVersions = db.Multiplexer.GetServer(db.Multiplexer.GetEndPoints()[0]).Version.CompareTo(new Version(7, 1, 242));
+        if (compareVersions < 0) // the server does not support the CLIENT SET NAME command
+            return false;
+        return db.Execute(CoreCommandBuilder.ClientSetInfo(attr, value)).OKtoBoolean();
     }
 }
