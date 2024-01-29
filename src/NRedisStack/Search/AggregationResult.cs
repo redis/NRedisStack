bce49c6a256e648689a9bcd40ef9e9d644eb7397
nimanikoo <nikoonazar.nima@gmail.com>
2024-01-29 23:09:41 +0330

    Sync Namespace and use scoped namespace and use operator instead if

--- src/NRedisStack/Search/AggregationResult.cs
+++ src/NRedisStack/Search/AggregationResult.cs
@@ -1,48 +1,47 @@
-﻿using StackExchange.Redis;
+﻿using NRedisStack.Search.Aggregation;
+using StackExchange.Redis;
 
-namespace NRedisStack.Search.Aggregation
+namespace NRedisStack.Search;
+
+public sealed class AggregationResult
 {
-    public sealed class AggregationResult
+    public long TotalResults { get; }
+    private readonly Dictionary<string, RedisValue>[] _results;
+    public long CursorId { get; }
+
+
+    internal AggregationResult(RedisResult result, long cursorId = -1)
     {
-        public long TotalResults { get; }
-        private readonly Dictionary<string, RedisValue>[] _results;
-        public long CursorId { get; }
+        var arr = (RedisResult[])result!;
 
+        // the first element is always the number of results
+        TotalResults = (long)arr[0];
 
-        internal AggregationResult(RedisResult result, long cursorId = -1)
+        _results = new Dictionary<string, RedisValue>[arr.Length - 1];
+        for (int i = 1; i < arr.Length; i++)
         {
-            var arr = (RedisResult[])result!;
-
-            // the first element is always the number of results
-            TotalResults = (long)arr[0];
-
-            _results = new Dictionary<string, RedisValue>[arr.Length - 1];
-            for (int i = 1; i < arr.Length; i++)
+            var raw = (RedisResult[])arr[i]!;
+            var cur = new Dictionary<string, RedisValue>();
+            for (int j = 0; j < raw.Length;)
             {
-                var raw = (RedisResult[])arr[i]!;
-                var cur = new Dictionary<string, RedisValue>();
-                for (int j = 0; j < raw.Length;)
-                {
-                    var key = (string)raw[j++]!;
-                    var val = raw[j++];
-                    if (val.Type == ResultType.MultiBulk)
-                        continue; // TODO: handle multi-bulk (maybe change to object?)
-                    cur.Add(key, (RedisValue)val);
-                }
-                _results[i - 1] = cur;
+                var key = (string)raw[j++]!;
+                var val = raw[j++];
+                if (val.Type == ResultType.MultiBulk)
+                    continue; // TODO: handle multi-bulk (maybe change to object?)
+                cur.Add(key, (RedisValue)val);
             }
-
-            CursorId = cursorId;
+            _results[i - 1] = cur;
         }
-        public IReadOnlyList<Dictionary<string, RedisValue>> GetResults() => _results;
 
-        public Dictionary<string, RedisValue>? this[int index]
-            => index >= _results.Length ? null : _results[index];
+        CursorId = cursorId;
+    }
+    public IReadOnlyList<Dictionary<string, RedisValue>> GetResults() => _results;
 
-        public Row GetRow(int index)
-        {
-            if (index >= _results.Length) return default;
-            return new Row(_results[index]);
-        }
+    public Dictionary<string, RedisValue>? this[int index]
+        => index >= _results.Length ? null : _results[index];
+
+    public Row GetRow(int index)
+    {
+        return index >= _results.Length ? default : new Row(_results[index]);
     }
 }
