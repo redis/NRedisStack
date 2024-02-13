fe18a3ba595085951852665f413323252deeaaa7
Gabriel Erzse <gabriel.erzse@gmail.com>
2024-02-13 12:10:12 +0200

    Add support for BZMPOP, BZPOPMIN and BZPOPMAX (#240)
    * Formatting and typos
    
    Fix some typos in the markdown files and run `dotnet format` on existing
    code.
    
    * Include documentation in the build
    
    Change the settings so that documentation is included in the build.
    
    * Add support for BZMPOP, BZPOPMIN and BZPOPMAX
    
    Add support for the BZMPOP, BZPOPMIN, BZPOPMAX commands.
    
    Issues #232, #233 and #234.
    
    These commands are blocking on the server, so they go against the current
    policy of the StackExchange.Redis library. Therefore make it obvious in
    the code documentation that attention must be given to the timeout in
    the connection multiplexer, client-side.
    
    The StackExchange.Redis library already defines a type for the payload
    returned by BZMPOP (which is the same as for ZMPOP), namely the
    SortedSetPopResult class. However, the constructor of that class is
    internal in the library, so we can't create instances of it. Therefore
    roll our out type for a <value, score> pair, and use Tuple to pair a key
    with a list of such <value, score> pairs.
    
    Instead of using Order to signal from which end of the sorted set to
    pop, define a MinMaxModifier enum, which more clearly expresses the
    intention and maps directly to the Redis command being executed.
    
    ---------
    
    Co-authored-by: Gabriel Erzse <gabriel.erzse@redis.com>

--- src/NRedisStack/CoreCommands/CoreCommandBuilder.cs
+++ src/NRedisStack/CoreCommands/CoreCommandBuilder.cs
@@ -1,6 +1,8 @@
 using NRedisStack.RedisStackCommands;
 using NRedisStack.Core.Literals;
 using NRedisStack.Core;
+using NRedisStack.Core.DataTypes;
+using StackExchange.Redis;
 
 namespace NRedisStack
 {
@@ -17,6 +19,57 @@ namespace NRedisStack
             };
 
             return new SerializedCommand(RedisCoreCommands.CLIENT, RedisCoreCommands.SETINFO, attrValue, value);
+        }
+
+        public static SerializedCommand BzmPop(double timeout, RedisKey[] keys, MinMaxModifier minMaxModifier, long? count)
+        {
+            if (keys.Length == 0)
+            {
+                throw new ArgumentException("At least one key must be provided.");
+            }
+
+            List<object> args = new List<object>();
+
+            args.Add(timeout);
+            args.Add(keys.Length);
+            args.AddRange(keys.Cast<object>());
+            args.Add(minMaxModifier == MinMaxModifier.Min ? CoreArgs.MIN : CoreArgs.MAX);
+
+            if (count != null)
+            {
+                args.Add(CoreArgs.COUNT);
+                args.Add(count);
+            }
+
+            return new SerializedCommand(RedisCoreCommands.BZMPOP, args);
+        }
+
+        public static SerializedCommand BzPopMin(RedisKey[] keys, double timeout)
+        {
+            if (keys.Length == 0)
+            {
+                throw new ArgumentException("At least one key must be provided.");
+            }
+
+            List<object> args = new List<object>();
+            args.AddRange(keys.Cast<object>());
+            args.Add(timeout);
+
+            return new SerializedCommand(RedisCoreCommands.BZPOPMIN, args);
+        }
+
+        public static SerializedCommand BzPopMax(RedisKey[] keys, double timeout)
+        {
+            if (keys.Length == 0)
+            {
+                throw new ArgumentException("At least one key must be provided.");
+            }
+
+            List<object> args = new List<object>();
+            args.AddRange(keys.Cast<object>());
+            args.Add(timeout);
+
+            return new SerializedCommand(RedisCoreCommands.BZPOPMAX, args);
         }
     }
 }
--- src/NRedisStack/CoreCommands/Literals/Commands.cs
+++ src/NRedisStack/CoreCommands/Literals/Commands.cs
@@ -3,8 +3,11 @@ namespace NRedisStack.Core.Literals
     /// <summary>
     /// Redis Core command literals
     /// </summary>
-    internal class RedisCoreCommands
+    internal static class RedisCoreCommands
     {
+        public const string BZMPOP = "BZMPOP";
+        public const string BZPOPMAX = "BZPOPMAX";
+        public const string BZPOPMIN = "BZPOPMIN";
         public const string CLIENT = "CLIENT";
         public const string SETINFO = "SETINFO";
     }
