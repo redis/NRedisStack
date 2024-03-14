e1eacb339ba456dab4de5a59da4c3747d082c426
nima nikoonazar <nikoonazar.nima@gmail.com>
2024-03-14 19:40:34 +0330

    Fix Typo and namespace also Set Private Access Modifier RedisFixture.cs for readonly strings

--- tests/NRedisStack.Tests/RedisFixture.cs
+++ tests/NRedisStack.Tests/RedisFixture.cs
@@ -1,68 +1,66 @@
 using StackExchange.Redis;
 
-namespace NRedisStack.Tests
+namespace NRedisStack.Tests;
+
+public class RedisFixture : IDisposable
 {
-    public class RedisFixture : IDisposable
+    // Set the environment variable to specify your own alternate host and port:
+    private readonly string redisStandalone = Environment.GetEnvironmentVariable("REDIS") ?? "localhost:6379";
+    private readonly string? redisCluster = Environment.GetEnvironmentVariable("REDIS_CLUSTER");
+    private readonly string? numRedisClusterNodesEnv = Environment.GetEnvironmentVariable("NUM_REDIS_CLUSTER_NODES");
+    public bool isEnterprise = Environment.GetEnvironmentVariable("IS_ENTERPRISE") == "true";
+    public bool isOSSCluster;
+
+    public RedisFixture()
     {
-        // Set the enviroment variable to specify your own alternet host and port:
-        readonly string redisStandalone = Environment.GetEnvironmentVariable("REDIS") ?? "localhost:6379";
-        readonly string? redisCluster = Environment.GetEnvironmentVariable("REDIS_CLUSTER");
-        readonly string? numRedisClusterNodesEnv = Environment.GetEnvironmentVariable("NUM_REDIS_CLUSTER_NODES");
-
-        public bool isEnterprise = Environment.GetEnvironmentVariable("IS_ENTERPRISE") == "true";
-        public bool isOSSCluster;
-
-        public RedisFixture()
+        ConfigurationOptions clusterConfig = new ConfigurationOptions
         {
-            ConfigurationOptions clusterConfig = new ConfigurationOptions
+            AsyncTimeout = 10000,
+            SyncTimeout = 10000
+        };
+        Redis = Connect(clusterConfig, out isOSSCluster);
+    }
+
+    public void Dispose()
+    {
+        Redis.Close();
+    }
+
+    public ConnectionMultiplexer Redis { get; }
+
+    public ConnectionMultiplexer CustomRedis(ConfigurationOptions configurationOptions, out bool isOssCluster)
+    {
+        return Connect(configurationOptions, out isOssCluster);
+    }
+
+    private ConnectionMultiplexer Connect(ConfigurationOptions configurationOptions, out bool isOssCluster)
+    {
+        // Redis Cluster
+        if (redisCluster != null && numRedisClusterNodesEnv != null)
+        {
+            // Split to host and port
+            string[] parts = redisCluster!.Split(':');
+            string host = parts[0];
+            int startPort = int.Parse(parts[1]);
+
+            var endpoints = new EndPointCollection(); // TODO: check if needed
+
+            configurationOptions.EndPoints.Clear();
+            int numRedisClusterNodes = int.Parse(numRedisClusterNodesEnv!);
+            for (int i = 0; i < numRedisClusterNodes; i++)
             {
-                AsyncTimeout = 10000,
-                SyncTimeout = 10000
-            };
-            Redis = Connect(clusterConfig, out isOSSCluster);
-        }
-
-        public void Dispose()
-        {
-            Redis.Close();
-        }
-
-        public ConnectionMultiplexer Redis { get; }
-
-        public ConnectionMultiplexer CustomRedis(ConfigurationOptions configurationOptions, out bool isOssCluster)
-        {
-            return Connect(configurationOptions, out isOssCluster);
-        }
-
-        private ConnectionMultiplexer Connect(ConfigurationOptions configurationOptions, out bool isOssCluster)
-        {
-            // Redis Cluster
-            if (redisCluster != null && numRedisClusterNodesEnv != null)
-            {
-                // Split to host and port
-                string[] parts = redisCluster!.Split(':');
-                string host = parts[0];
-                int startPort = int.Parse(parts[1]);
-
-                var endpoints = new EndPointCollection(); // TODO: checl if needed
-
-                configurationOptions.EndPoints.Clear();
-                int numRedisClusterNodes = int.Parse(numRedisClusterNodesEnv!);
-                for (int i = 0; i < numRedisClusterNodes; i++)
-                {
-                    configurationOptions.EndPoints.Add(host, startPort + i);
-                }
-
-                isOssCluster = true;
-                return ConnectionMultiplexer.Connect(configurationOptions);
+                configurationOptions.EndPoints.Add(host, startPort + i);
             }
 
-            // Redis Standalone
-            configurationOptions.EndPoints.Clear();
-            configurationOptions.EndPoints.Add($"{redisStandalone}");
-
-            isOssCluster = false;
+            isOssCluster = true;
             return ConnectionMultiplexer.Connect(configurationOptions);
         }
+
+        // Redis Standalone
+        configurationOptions.EndPoints.Clear();
+        configurationOptions.EndPoints.Add($"{redisStandalone}");
+
+        isOssCluster = false;
+        return ConnectionMultiplexer.Connect(configurationOptions);
     }
 }
--- tests/NRedisStack.Tests/TransactionsTests.cs
+++ tests/NRedisStack.Tests/TransactionsTests.cs
@@ -4,133 +4,132 @@ using StackExchange.Redis;
 using System.Text.Json;
 using Xunit;
 
-namespace NRedisStack.Tests
+namespace NRedisStack.Tests;
+
+public class TransactionTests : AbstractNRedisStackTest, IDisposable
 {
-    public class TransactionTests : AbstractNRedisStackTest, IDisposable
+    private readonly string key = "TRX_TESTS";
+
+    public TransactionTests(RedisFixture redisFixture) : base(redisFixture)
     {
-        private readonly string key = "TRX_TESTS";
+    }
 
-        public TransactionTests(RedisFixture redisFixture) : base(redisFixture)
-        {
-        }
+    [Fact]
+    public void TestJsonTransaction()
+    {
+        IDatabase db = redisFixture.Redis.GetDatabase();
+        db.Execute("FLUSHALL");
+        var transaction = new Transaction(db);
+        string jsonPerson = JsonSerializer.Serialize(new Person { Name = "Shachar", Age = 23 });
+        var setResponse = transaction.Json.SetAsync(key, "$", jsonPerson);
+        var getResponse = transaction.Json.GetAsync(key);
 
-        [Fact]
-        public void TestJsonTransaction()
-        {
-            IDatabase db = redisFixture.Redis.GetDatabase();
-            db.Execute("FLUSHALL");
-            var transaction = new Transaction(db);
-            string jsonPerson = JsonSerializer.Serialize(new Person { Name = "Shachar", Age = 23 });
-            var setResponse = transaction.Json.SetAsync(key, "$", jsonPerson);
-            var getResponse = transaction.Json.GetAsync(key);
+        transaction.Execute();
 
-            transaction.Execute();
+        setResponse.Wait();
+        getResponse.Wait();
 
-            setResponse.Wait();
-            getResponse.Wait();
+        Assert.True(setResponse.Result);
+        Assert.Equal("{\"Name\":\"Shachar\",\"Age\":23}", getResponse.Result.ToString());
+    }
 
-            Assert.True(setResponse.Result);
-            Assert.Equal("{\"Name\":\"Shachar\",\"Age\":23}", getResponse.Result.ToString());
-        }
+    [SkipIfRedis(Comparison.GreaterThanOrEqual, "7.1.242")]
+    [Obsolete]
+    public void TestModulesTransaction()
+    {
+        IDatabase db = redisFixture.Redis.GetDatabase();
+        db.Execute("FLUSHALL");
+        var tran = new Transaction(db);
 
-        [SkipIfRedis(Comparison.GreaterThanOrEqual, "7.1.242")]
-        [Obsolete]
-        public void TestModulesTransaction()
-        {
-            IDatabase db = redisFixture.Redis.GetDatabase();
-            db.Execute("FLUSHALL");
-            var tran = new Transaction(db);
+        _ = tran.Bf.ReserveAsync("bf-key", 0.001, 100);
+        _ = tran.Bf.AddAsync("bf-key", "1");
+        _ = tran.Cms.InitByDimAsync("cms-key", 100, 5);
+        _ = tran.Cf.ReserveAsync("cf-key", 100);
+        _ = tran.Graph.QueryAsync("graph-key", "CREATE ({name:'shachar',age:23})");
+        _ = tran.Json.SetAsync("json-key", "$", "{}");
+        _ = tran.Ft.CreateAsync("ft-key", new FTCreateParams(), new Schema().AddTextField("txt"));
+        _ = tran.Tdigest.CreateAsync("tdigest-key", 100);
+        _ = tran.Ts.CreateAsync("ts-key", 100);
+        _ = tran.TopK.ReserveAsync("topk-key", 100, 100, 100);
 
-            _ = tran.Bf.ReserveAsync("bf-key", 0.001, 100);
-            _ = tran.Bf.AddAsync("bf-key", "1");
-            _ = tran.Cms.InitByDimAsync("cms-key", 100, 5);
-            _ = tran.Cf.ReserveAsync("cf-key", 100);
-            _ = tran.Graph.QueryAsync("graph-key", "CREATE ({name:'shachar',age:23})");
-            _ = tran.Json.SetAsync("json-key", "$", "{}");
-            _ = tran.Ft.CreateAsync("ft-key", new FTCreateParams(), new Schema().AddTextField("txt"));
-            _ = tran.Tdigest.CreateAsync("tdigest-key", 100);
-            _ = tran.Ts.CreateAsync("ts-key", 100);
-            _ = tran.TopK.ReserveAsync("topk-key", 100, 100, 100);
+        Assert.False(db.KeyExists("bf-key"));
+        Assert.False(db.KeyExists("cms-key"));
+        Assert.False(db.KeyExists("cf-key"));
+        Assert.False(db.KeyExists("graph-key"));
+        Assert.False(db.KeyExists("json-key"));
+        Assert.Empty(db.FT()._List());
+        Assert.False(db.KeyExists("tdigest-key"));
+        Assert.False(db.KeyExists("ts-key"));
+        Assert.False(db.KeyExists("topk-key"));
 
-            Assert.False(db.KeyExists("bf-key"));
-            Assert.False(db.KeyExists("cms-key"));
-            Assert.False(db.KeyExists("cf-key"));
-            Assert.False(db.KeyExists("graph-key"));
-            Assert.False(db.KeyExists("json-key"));
-            Assert.Empty(db.FT()._List());
-            Assert.False(db.KeyExists("tdigest-key"));
-            Assert.False(db.KeyExists("ts-key"));
-            Assert.False(db.KeyExists("topk-key"));
+        tran.Execute();
 
-            tran.Execute();
+        Assert.True(db.KeyExists("bf-key"));
+        Assert.True(db.KeyExists("cms-key"));
+        Assert.True(db.KeyExists("cf-key"));
+        Assert.True(db.KeyExists("graph-key"));
+        Assert.True(db.KeyExists("json-key"));
+        Assert.True(db.FT()._List().Length == 1);
+        Assert.True(db.KeyExists("tdigest-key"));
+        Assert.True(db.KeyExists("ts-key"));
+        Assert.True(db.KeyExists("topk-key"));
 
-            Assert.True(db.KeyExists("bf-key"));
-            Assert.True(db.KeyExists("cms-key"));
-            Assert.True(db.KeyExists("cf-key"));
-            Assert.True(db.KeyExists("graph-key"));
-            Assert.True(db.KeyExists("json-key"));
-            Assert.True(db.FT()._List().Length == 1);
-            Assert.True(db.KeyExists("tdigest-key"));
-            Assert.True(db.KeyExists("ts-key"));
-            Assert.True(db.KeyExists("topk-key"));
+        Assert.True(db.BF().Exists("bf-key", "1"));
+        Assert.True(db.CMS().Info("cms-key").Width == 100);
+        Assert.True(db.CF().Info("cf-key").Size > 0);
+        Assert.True(db.GRAPH().List().Count > 0);
+        Assert.False(db.JSON().Get("json-key").IsNull);
+        Assert.NotNull(db.FT().Info("ft-key"));
+        Assert.NotNull(db.TDIGEST().Info("tdigest-key"));
+        Assert.NotNull(db.TS().Info("ts-key"));
+        Assert.NotNull(db.TOPK().Info("topk-key"));
+    }
 
-            Assert.True(db.BF().Exists("bf-key", "1"));
-            Assert.True(db.CMS().Info("cms-key").Width == 100);
-            Assert.True(db.CF().Info("cf-key").Size > 0);
-            Assert.True(db.GRAPH().List().Count > 0);
-            Assert.False(db.JSON().Get("json-key").IsNull);
-            Assert.NotNull(db.FT().Info("ft-key"));
-            Assert.NotNull(db.TDIGEST().Info("tdigest-key"));
-            Assert.NotNull(db.TS().Info("ts-key"));
-            Assert.NotNull(db.TOPK().Info("topk-key"));
-        }
+    [SkipIfRedis(Is.OSSCluster, Is.Enterprise)]
+    [Obsolete]
+    public void TestModulesTransactionWithoutGraph()
+    {
+        IDatabase db = redisFixture.Redis.GetDatabase();
+        db.Execute("FLUSHALL");
+        var tran = new Transaction(db);
 
-        [SkipIfRedis(Is.OSSCluster, Is.Enterprise)]
-        [Obsolete]
-        public void TestModulesTransactionWithoutGraph()
-        {
-            IDatabase db = redisFixture.Redis.GetDatabase();
-            db.Execute("FLUSHALL");
-            var tran = new Transaction(db);
+        _ = tran.Bf.ReserveAsync("bf-key", 0.001, 100);
+        _ = tran.Bf.AddAsync("bf-key", "1");
+        _ = tran.Cms.InitByDimAsync("cms-key", 100, 5);
+        _ = tran.Cf.ReserveAsync("cf-key", 100);
+        _ = tran.Json.SetAsync("json-key", "$", "{}");
+        _ = tran.Ft.CreateAsync("ft-key", new FTCreateParams(), new Schema().AddTextField("txt"));
+        _ = tran.Tdigest.CreateAsync("tdigest-key", 100);
+        _ = tran.Ts.CreateAsync("ts-key", 100);
+        _ = tran.TopK.ReserveAsync("topk-key", 100, 100, 100);
 
-            _ = tran.Bf.ReserveAsync("bf-key", 0.001, 100);
-            _ = tran.Bf.AddAsync("bf-key", "1");
-            _ = tran.Cms.InitByDimAsync("cms-key", 100, 5);
-            _ = tran.Cf.ReserveAsync("cf-key", 100);
-            _ = tran.Json.SetAsync("json-key", "$", "{}");
-            _ = tran.Ft.CreateAsync("ft-key", new FTCreateParams(), new Schema().AddTextField("txt"));
-            _ = tran.Tdigest.CreateAsync("tdigest-key", 100);
-            _ = tran.Ts.CreateAsync("ts-key", 100);
-            _ = tran.TopK.ReserveAsync("topk-key", 100, 100, 100);
+        Assert.False(db.KeyExists("bf-key"));
+        Assert.False(db.KeyExists("cms-key"));
+        Assert.False(db.KeyExists("cf-key"));
+        Assert.False(db.KeyExists("json-key"));
+        Assert.Empty(db.FT()._List());
+        Assert.False(db.KeyExists("tdigest-key"));
+        Assert.False(db.KeyExists("ts-key"));
+        Assert.False(db.KeyExists("topk-key"));
 
-            Assert.False(db.KeyExists("bf-key"));
-            Assert.False(db.KeyExists("cms-key"));
-            Assert.False(db.KeyExists("cf-key"));
-            Assert.False(db.KeyExists("json-key"));
-            Assert.Empty(db.FT()._List());
-            Assert.False(db.KeyExists("tdigest-key"));
-            Assert.False(db.KeyExists("ts-key"));
-            Assert.False(db.KeyExists("topk-key"));
+        tran.Execute();
 
-            tran.Execute();
+        Assert.True(db.KeyExists("bf-key"));
+        Assert.True(db.KeyExists("cms-key"));
+        Assert.True(db.KeyExists("cf-key"));
+        Assert.True(db.KeyExists("json-key"));
+        Assert.True(db.FT()._List().Length == 1);
+        Assert.True(db.KeyExists("tdigest-key"));
+        Assert.True(db.KeyExists("ts-key"));
+        Assert.True(db.KeyExists("topk-key"));
 
-            Assert.True(db.KeyExists("bf-key"));
-            Assert.True(db.KeyExists("cms-key"));
-            Assert.True(db.KeyExists("cf-key"));
-            Assert.True(db.KeyExists("json-key"));
-            Assert.True(db.FT()._List().Length == 1);
-            Assert.True(db.KeyExists("tdigest-key"));
-            Assert.True(db.KeyExists("ts-key"));
-            Assert.True(db.KeyExists("topk-key"));
-
-            Assert.True(db.BF().Exists("bf-key", "1"));
-            Assert.True(db.CMS().Info("cms-key").Width == 100);
-            Assert.True(db.CF().Info("cf-key").Size > 0);
-            Assert.False(db.JSON().Get("json-key").IsNull);
-            Assert.NotNull(db.FT().Info("ft-key"));
-            Assert.NotNull(db.TDIGEST().Info("tdigest-key"));
-            Assert.NotNull(db.TS().Info("ts-key"));
-            Assert.NotNull(db.TOPK().Info("topk-key"));
-        }
+        Assert.True(db.BF().Exists("bf-key", "1"));
+        Assert.True(db.CMS().Info("cms-key").Width == 100);
+        Assert.True(db.CF().Info("cf-key").Size > 0);
+        Assert.False(db.JSON().Get("json-key").IsNull);
+        Assert.NotNull(db.FT().Info("ft-key"));
+        Assert.NotNull(db.TDIGEST().Info("tdigest-key"));
+        Assert.NotNull(db.TS().Info("ts-key"));
+        Assert.NotNull(db.TOPK().Info("topk-key"));
     }
 }
