15e3e441588d9e414c7b4b72cf0eb0cc0eb5d70a
nima nikoonazar <nikoonazar.nima@gmail.com>
2024-03-14 19:52:36 +0330

    Fix Typo and Change Namespace to scoped AbstractNRedisStackTest.cs and Person.cs

--- tests/NRedisStack.Tests/AbstractNRedisStackTest.cs
+++ tests/NRedisStack.Tests/AbstractNRedisStackTest.cs
@@ -1,58 +1,56 @@
 
 using NRedisStack.DataTypes;
-using StackExchange.Redis;
 using System.Runtime.CompilerServices;
 using Xunit;
-namespace NRedisStack.Tests
+namespace NRedisStack.Tests;
+
+public abstract class AbstractNRedisStackTest : IClassFixture<RedisFixture>, IAsyncLifetime
 {
-    public abstract class AbstractNRedisStackTest : IClassFixture<RedisFixture>, IAsyncLifetime
+    protected internal RedisFixture redisFixture;
+
+    protected internal AbstractNRedisStackTest(RedisFixture redisFixture) => this.redisFixture = redisFixture;
+
+    private List<string> keyNames = new List<string>();
+
+    protected internal string CreateKeyName([CallerMemberName] string memberName = "") => CreateKeyNames(1, memberName)[0];
+
+    protected internal string[] CreateKeyNames(int count, [CallerMemberName] string memberName = "")
     {
-        protected internal RedisFixture redisFixture;
+        if (count < 1) throw new ArgumentOutOfRangeException(nameof(count), "Must be greater than zero.");
 
-        protected internal AbstractNRedisStackTest(RedisFixture redisFixture) => this.redisFixture = redisFixture;
-
-        private List<string> keyNames = new List<string>();
-
-        protected internal string CreateKeyName([CallerMemberName] string memberName = "") => CreateKeyNames(1, memberName)[0];
-
-        protected internal string[] CreateKeyNames(int count, [CallerMemberName] string memberName = "")
+        var newKeys = new string[count];
+        for (var i = 0; i < count; i++)
         {
-            if (count < 1) throw new ArgumentOutOfRangeException(nameof(count), "Must be greater than zero.");
-
-            var newKeys = new string[count];
-            for (var i = 0; i < count; i++)
-            {
-                newKeys[i] = $"{GetType().Name}:{memberName}:{i}";
-            }
-
-            keyNames.AddRange(newKeys);
-
-            return newKeys;
+            newKeys[i] = $"{GetType().Name}:{memberName}:{i}";
         }
 
-        protected internal static List<TimeSeriesTuple> ReverseData(List<TimeSeriesTuple> data)
-        {
-            var tuples = new List<TimeSeriesTuple>(data.Count);
-            for (var i = data.Count - 1; i >= 0; i--)
-            {
-                tuples.Add(data[i]);
-            }
+        keyNames.AddRange(newKeys);
 
-            return tuples;
+        return newKeys;
+    }
+
+    protected internal static List<TimeSeriesTuple> ReverseData(List<TimeSeriesTuple> data)
+    {
+        var tuples = new List<TimeSeriesTuple>(data.Count);
+        for (var i = data.Count - 1; i >= 0; i--)
+        {
+            tuples.Add(data[i]);
         }
 
-        public Task InitializeAsync() => Task.CompletedTask;
+        return tuples;
+    }
 
-        public void Dispose()
-        {
-            redisFixture.Redis.GetDatabase().ExecuteBroadcast("FLUSHALL");
-        }
+    public Task InitializeAsync() => Task.CompletedTask;
 
-        public async Task DisposeAsync()
-        {
-            var redis = redisFixture.Redis.GetDatabase();
-            // await redis.KeyDeleteAsync(keyNames.Select(i => (RedisKey)i).ToArray());
-            await redis.ExecuteBroadcastAsync("FLUSHALL");
-        }
+    public void Dispose()
+    {
+        redisFixture.Redis.GetDatabase().ExecuteBroadcast("FLUSHALL");
+    }
+
+    public async Task DisposeAsync()
+    {
+        var redis = redisFixture.Redis.GetDatabase();
+        // await redis.KeyDeleteAsync(keyNames.Select(i => (RedisKey)i).ToArray());
+        await redis.ExecuteBroadcastAsync("FLUSHALL");
     }
 }
