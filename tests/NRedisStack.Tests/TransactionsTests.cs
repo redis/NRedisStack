8ff82fd911bd8e22023f9f36f080de6f130e9bab
shacharPash <shachar.pashchur@redis.com>
2024-03-17 17:35:59 +0200

    delete tests

--- tests/NRedisStack.Tests/TransactionsTests.cs
+++ tests/NRedisStack.Tests/TransactionsTests.cs
@@ -131,56 +131,6 @@ namespace NRedisStack.Tests
             Assert.NotNull(db.TOPK().Info("topk-key"));
         }
 
-        [SkipIfRedis(Is.OSSCluster, Is.Enterprise)]
-        public void TestJsonExpire()
-        {
-            IDatabase db = redisFixture.Redis.GetDatabase();
-            db.Execute("FLUSHALL");
-            var tran = new Transaction(db);
-            var json = db.JSON();
-
-            // var setRes = tran.Db.ExecuteAsync("JSON.SET", "json-key", "$", "{123}");
-            var setRes = tran.Json.SetAsync("json-key", "$", "{111}");
-            var set1Res = tran.Json.SetAsync("json-key", "$", "{111}");
-            var set2Res = tran.Json.SetAsync("json-key2", "$", "{222}");
-            var set3Res = tran.Json.SetAsync("json-key3", "$", "{333}");
-            var set4Res = tran.Json.SetAsync("json-key4", "$", "{444}");
-
-
-
-            var expireRes = tran.Db.KeyExpireAsync("json-key", TimeSpan.FromSeconds(10));
-            tran.ExecuteAsync();
-
-            var getBeforeExpire = json.Get("json-key");
-            Assert.False(getBeforeExpire.IsNull);
-
-            Thread.Sleep(TimeSpan.FromSeconds(11));
-            var getAfterExpire = json.Get("json-key");
-            Assert.True(getAfterExpire.IsNull);
-        }
-
-        [SkipIfRedis(Is.OSSCluster, Is.Enterprise)]
-        public void TestJsonExpire2()
-        {
-            IDatabase db = redisFixture.Redis.GetDatabase();
-            db.Execute("FLUSHALL");
-            var tran = db.CreateTransaction();
-            var json = db.JSON();
-
-            tran.ExecuteAsync("JSON.SET", "json-key", "$", "{123}");
-            tran.ExecuteAsync("JSON.SET", "json-key2", "$", "{456}");
-            tran.ExecuteAsync("EXPIRE", "json-key", 10);
-
-            tran.Execute();
-
-            var getBeforeExpire = json.Get("json-key");
-            Assert.False(getBeforeExpire.IsNull);
-
-            Thread.Sleep(TimeSpan.FromSeconds(11));
-            var getAfterExpire = json.Get("json-key");
-            Assert.True(getAfterExpire.IsNull);
-        }
-
         [Fact]
         public async Task TestJsonTransactionExpireAsync()
         {
