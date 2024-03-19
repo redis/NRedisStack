4159b238cf8741576d43678221d42e45b654e656
Gabriel Erzse <gabriel.erzse@redis.com>
2024-03-19 10:00:35 +0200

    Revert ToString() in code snippets
    Trying to keep code snippets as short as possible, since they get
    published on redis.io as documentation. Hence reverting the addition of
    the `ToString()` calls in some places.

--- tests/Doc/StringSnippets.cs
+++ tests/Doc/StringSnippets.cs
@@ -27,7 +27,7 @@ public class StringSnippets
 
         // STEP_START set_get
         var res1 = db.StringSet("bike:1", "Deimos");
-        Console.WriteLine(res1.ToString()); // true
+        Console.WriteLine(res1); // true
         var res2 = db.StringGet("bike:1");
         Console.WriteLine(res2); // Deimos
         // STEP_END
@@ -39,10 +39,10 @@ public class StringSnippets
 
         //STEP_START setnx_xx
         var res3 = db.StringSet("bike:1", "bike", when: When.NotExists);
-        Console.WriteLine(res3.ToString()); // false
+        Console.WriteLine(res3); // false
         Console.WriteLine(db.StringGet("bike:1"));
         var res4 = db.StringSet("bike:1", "bike", when: When.Exists);
-        Console.WriteLine(res4.ToString()); // true
+        Console.WriteLine(res4); // true
         //STEP_END
 
         //REMOVE_START
@@ -55,9 +55,9 @@ public class StringSnippets
         {
             new ("bike:1", "Deimos"), new("bike:2", "Ares"), new("bike:3", "Vanth")
         });
-        Console.WriteLine(res5.ToString());
+        Console.WriteLine(res5);
         var res6 = db.StringGet(new RedisKey[] { "bike:1", "bike:2", "bike:3" });
-        Console.WriteLine(res6.ToString());
+        Console.WriteLine(res6);
         //STEP_END
 
         //REMOVE_START
@@ -68,9 +68,9 @@ public class StringSnippets
         //STEP_START incr
         db.StringSet("total_crashes", 0);
         var res7 = db.StringIncrement("total_crashes");
-        Console.WriteLine(res7.ToString()); // 1
+        Console.WriteLine(res7); // 1
         var res8 = db.StringIncrement("total_crashes", 10);
-        Console.WriteLine(res8.ToString());
+        Console.WriteLine(res8);
         //STEP_END
 
         //REMOVE_START
