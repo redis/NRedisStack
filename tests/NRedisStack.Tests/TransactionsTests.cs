a4ba2540435175ffdfaee0a3f20a834cf092603a
shacharPash <shachar.pashchur@redis.com>
2024-04-02 08:10:04 +0300

    try to fix TestJsonTransaction

--- tests/NRedisStack.Tests/TransactionsTests.cs
+++ tests/NRedisStack.Tests/TransactionsTests.cs
@@ -15,22 +15,22 @@ public class TransactionTests : AbstractNRedisStackTest, IDisposable
     }
 
     [Fact]
-    public void TestJsonTransaction()
+    public async void TestJsonTransaction()
     {
         IDatabase db = redisFixture.Redis.GetDatabase();
         db.Execute("FLUSHALL");
         var transaction = new Transaction(db);
         string jsonPerson = JsonSerializer.Serialize(new Person { Name = "Shachar", Age = 23 });
-        var setResponse = transaction.Json.SetAsync(key, "$", jsonPerson);
-        var getResponse = transaction.Json.GetAsync(key);
+        var setResponse = await transaction.Json.SetAsync(key, "$", jsonPerson);
+        var getResponse = await transaction.Json.GetAsync(key);
 
         transaction.Execute();
 
-        setResponse.Wait();
-        getResponse.Wait();
+        // setResponse.Wait();
+        // getResponse.Wait();
 
-        Assert.True(setResponse.Result);
-        Assert.Equal("{\"Name\":\"Shachar\",\"Age\":23}", getResponse.Result.ToString());
+        Assert.True(setResponse);
+        Assert.Equal("{\"Name\":\"Shachar\",\"Age\":23}", getResponse.ToString());
     }
 
     [SkipIfRedis(Comparison.GreaterThanOrEqual, "7.1.242")]
