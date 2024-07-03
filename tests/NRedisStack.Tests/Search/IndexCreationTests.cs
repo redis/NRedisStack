ebc116375d0d824dc1602b229907d048bcceee38
atakavci <58048133+atakavci@users.noreply.github.com>
2024-07-03 19:22:34 +0300

    Verify library support for float16 and bfloat15 types in vector fields (#317)
    * adding tests for bfloat16
    
    * disable profile tests for 7.3.240
    
    * remove interop

--- tests/NRedisStack.Tests/Search/IndexCreationTests.cs
+++ tests/NRedisStack.Tests/Search/IndexCreationTests.cs
@@ -2,7 +2,6 @@ using StackExchange.Redis;
 using NRedisStack.Search;
 using NRedisStack.RedisStackCommands;
 using Xunit;
-using NRedisStack.Search.Literals;
 using NetTopologySuite.Geometries;
 
 namespace NRedisStack.Tests.Search;
@@ -122,5 +121,46 @@ public class IndexCreationTests : AbstractNRedisStackTest, IDisposable
         Assert.Equal(1, result.TotalResults);
         Assert.Equal("hashWithEmptyFields", result.Documents[0].Id);
 
+    }
+
+    [SkipIfRedis(Is.OSSCluster, Comparison.LessThan, "7.3.240")]
+    public void TestCreateFloat16VectorField()
+    {
+        IDatabase db = redisFixture.Redis.GetDatabase();
+        db.Execute("FLUSHALL");
+        var ft = db.FT(2);
+        var schema = new Schema().AddVectorField("v", Schema.VectorField.VectorAlgo.FLAT, new Dictionary<string, object>()
+        {
+            ["TYPE"] = "FLOAT16",
+            ["DIM"] = "5",
+            ["DISTANCE_METRIC"] = "L2",
+        }).AddVectorField("v2", Schema.VectorField.VectorAlgo.FLAT, new Dictionary<string, object>()
+        {
+            ["TYPE"] = "BFLOAT16",
+            ["DIM"] = "4",
+            ["DISTANCE_METRIC"] = "L2",
+        });
+        Assert.True(ft.Create("idx", new FTCreateParams(), schema));
+
+        short[] vec1 = new short[] { 2, 1, 2, 2, 2 };
+        byte[] vec1ToBytes = new byte[vec1.Length * sizeof(short)];
+        Buffer.BlockCopy(vec1, 0, vec1ToBytes, 0, vec1ToBytes.Length);
+
+        short[] vec2 = new short[] { 1, 2, 2, 2 };
+        byte[] vec2ToBytes = new byte[vec2.Length * sizeof(short)];
+        Buffer.BlockCopy(vec2, 0, vec2ToBytes, 0, vec2ToBytes.Length);
+
+        var entries = new HashEntry[] { new HashEntry("v", vec1ToBytes), new HashEntry("v2", vec2ToBytes) };
+        db.HashSet("a", entries);
+        db.HashSet("b", entries);
+        db.HashSet("c", entries);
+
+        var q = new Query("*=>[KNN 2 @v $vec]").ReturnFields("__v_score");
+        var res = ft.Search("idx", q.AddParam("vec", vec1ToBytes));
+        Assert.Equal(2, res.TotalResults);
+
+        q = new Query("*=>[KNN 2 @v2 $vec]").ReturnFields("__v_score");
+        res = ft.Search("idx", q.AddParam("vec", vec2ToBytes));
+        Assert.Equal(2, res.TotalResults);
     }
 }
