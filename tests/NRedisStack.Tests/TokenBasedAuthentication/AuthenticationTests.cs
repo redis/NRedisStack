11c8784ed374b0fe4fda7e5e506f14ca1004f383
atakavci <a_takavci@yahoo.com>
2024-10-10 15:10:12 +0300

    some clean up and test execution optimization

--- tests/NRedisStack.Tests/TokenBasedAuthentication/AuthenticationTests.cs
+++ tests/NRedisStack.Tests/TokenBasedAuthentication/AuthenticationTests.cs
@@ -18,16 +18,12 @@ namespace NRedisStack.Tests.TokenBasedAuthentication
         [TargetEnvironment("standalone-entraid-acl")]
         public void TestTokenBasedAuthentication()
         {
-            Assert.True(new FaultInjectorClient().TriggerActionAsync("enable_entraid", new Dictionary<string, object>()).Wait(5000), "Entraid could not be enabled this time!!!");
-            // NOTE: ConnectionMultiplexer instances should be as long-lived as possible. Ideally a single ConnectionMultiplexer per cache is reused over the lifetime of the client application process.
-            ConnectionMultiplexer? connectionMultiplexer = null;
-            // StringWriter connectionLog = new();
 
             var configurationOptions = new ConfigurationOptions().ConfigureForAzureWithTokenCredentialAsync(new DefaultAzureCredential()).Result!;
             configurationOptions.Ssl = false;
             configurationOptions.AbortOnConnectFail = true; // Fail fast for the purposes of this sample. In production code, this should remain false to retry connections on startup
 
-            connectionMultiplexer = redisFixture.GetConnectionById(configurationOptions, "standalone-entraid-acl");
+            ConnectionMultiplexer?connectionMultiplexer = redisFixture.GetConnectionById(configurationOptions, "standalone-entraid-acl");
 
             IDatabase db = connectionMultiplexer.GetDatabase();
 
@@ -52,12 +48,6 @@ namespace NRedisStack.Tests.TokenBasedAuthentication
             SearchResult res1 = ft.Search(alias, new Query("*").ReturnFields(field));
             Assert.Equal(1, res1.TotalResults);
             Assert.Equal(value, res1.Documents[0][field]);
-        }
-
-        [Fact]
-        public void DummyTest()
-        {
-            Assert.True(true);
         }
     }
 }
