0cb82dc49609f55c282be66bf0fd2ff2106b7f63
Andy Stark <andrew.stark@redis.com>
2025-02-24 10:45:04 +0000

    DOC-4733 updated testing code

--- tests/Doc/GeoIndexExample.cs
+++ tests/Doc/GeoIndexExample.cs
@@ -9,7 +9,6 @@ using StackExchange.Redis;
 
 // REMOVE_START
 using NRedisStack.Tests;
-using NetTopologySuite.Geometries;
 
 namespace Doc;
 [Collection("DocsTests")]
@@ -17,11 +16,22 @@ namespace Doc;
 
 // HIDE_START
 public class GeoIndexExample
+// REMOVE_START
+: AbstractNRedisStackTest, IDisposable
+// REMOVE_END
 {
+    // REMOVE_START
+    public GeoIndexExample(EndpointsFixture fixture) : base(fixture) { }
 
-    [SkipIfRedis(Is.OSSCluster)]
+    [SkippableFact]
+    // REMOVE_END
     public void run()
     {
+        //REMOVE_START
+        // This is needed because we're constructing ConfigurationOptions in the test before calling GetConnection
+        SkipIfTargetConnectionDoesNotExist(EndpointsFixture.Env.Standalone);
+        var _ = GetCleanDatabase(EndpointsFixture.Env.Standalone);
+        //REMOVE_END
         var muxer = ConnectionMultiplexer.Connect("localhost:6379");
         var db = muxer.GetDatabase();
         //REMOVE_START
