6c06c5cc4307a18ad8bfe65bb5eac5effe16da16
Marc Gravell <marc.gravell@gmail.com>
2026-02-03 10:57:36 +0000

    fix #457

--- src/NRedisStack/Search/Row.cs
+++ src/NRedisStack/Search/Row.cs
@@ -1,8 +1,9 @@
-﻿using StackExchange.Redis;
+﻿using System.Collections;
+using StackExchange.Redis;
 
 namespace NRedisStack.Search.Aggregation;
 
-public readonly struct Row
+public readonly struct Row : IEnumerable<KeyValuePair<string, object>>
 {
     private readonly Dictionary<string, object> _fields;
 
@@ -18,4 +19,30 @@ public readonly struct Row
     public string? GetString(string key) => _fields.TryGetValue(key, out var result) ? result.ToString() : default;
     public long GetLong(string key) => _fields.TryGetValue(key, out var result) ? (long)(RedisValue)result : default;
     public double GetDouble(string key) => _fields.TryGetValue(key, out var result) ? (double)(RedisValue)result : default;
+
+    /// <summary>
+    /// Access the fields as a sequence of key/value pairs.
+    /// </summary>
+    public Enumerator GetEnumerator() => new(in this);
+
+    public struct Enumerator : IEnumerator<KeyValuePair<string, object>>
+    {
+        private Dictionary<string, object>.Enumerator _enumerator;
+        internal Enumerator(in Row row) => _enumerator = row._fields?.GetEnumerator() ?? default;
+
+        /// <inheritdoc/>
+        public bool MoveNext() => _enumerator.MoveNext();
+
+        /// <inheritdoc/>
+        public KeyValuePair<string, object> Current => _enumerator.Current;
+
+        void IEnumerator.Reset() => throw new NotSupportedException();
+        
+        object IEnumerator.Current => Current;
+
+        void IDisposable.Dispose() => _enumerator.Dispose();
+    }
+
+    IEnumerator<KeyValuePair<string, object>> IEnumerable<KeyValuePair<string, object>>.GetEnumerator() => GetEnumerator();
+    IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
 }
